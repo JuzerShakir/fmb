e575611186a90f0c849926d3b19675638c351d6a
Juzer Shakir <juzershakir.webdev@gmail.com>
2022-12-16 20:33:41 +0530

    create test suit for thaali model

---
+++ spec/models/thaali_spec.rb
@@ -0,0 +1,17 @@
+require "rails_helper"
+
+RSpec.describe "Thaali", type: :model do
+    context "validations of" do
+        context "number attribute" do
+            it { should validate_numericality_of(:number).only_integer }
+            it { should validate_uniqueness_of(:number) }
+            it { should validate_presence_of(:number) }
+            it { should validate_numericality_of(:number).is_greater_than(0) }
+        end
+
+        context "size attribute" do
+            it { should define_enum_for(:size).with_values([:small, :medium, :large]) }
+            it { should validate_presence_of(:size) }
+        end
+    end
+end
