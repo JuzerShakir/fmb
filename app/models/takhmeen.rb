cd7491907ddecc5a4d33c699bacc6f9f9f83e376
Juzer Shakir <juzershakir.webdev@gmail.com>
2022-12-17 11:51:46 +0530

    create validation test suite and passing test for year attribute

--- app/models/takhmeen.rb
+++ app/models/takhmeen.rb
@@ -1,3 +1,7 @@
 class Takhmeen < ApplicationRecord
   belongs_to :thaali
+
+  validates_presence_of :year
+  validates_numericality_of :year, only_integer: true, greater_than_or_equal_to: 2022
+  validates_uniqueness_of :year, { scope: :thaali_id }
 end
--- spec/models/takhmeen_spec.rb
+++ spec/models/takhmeen_spec.rb
@@ -4,4 +4,18 @@ RSpec.describe Takhmeen, type: :model do
     context "association" do
         it { should belong_to(:thaali) }
     end
+
+    context "validation of attribute" do
+        subject { build(:takhmeen) }
+
+        context "year" do
+            it { should validate_presence_of(:year) }
+            it { should validate_numericality_of(:year).only_integer }
+
+            it { should validate_numericality_of(:year).is_greater_than_or_equal_to(2022) }
+
+            it { should validate_uniqueness_of(:year).scoped_to(:thaali_id) }
+        end
+
+    end
 end
