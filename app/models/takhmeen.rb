c037671e26619ef37f4982c415cdce98052f374e
Juzer Shakir <juzershakir.webdev@gmail.com>
2022-12-17 12:10:06 +0530

    create validation test suite and passing test for paid attribute

--- app/models/takhmeen.rb
+++ app/models/takhmeen.rb
@@ -1,10 +1,12 @@
 class Takhmeen < ApplicationRecord
   belongs_to :thaali
 
-  validates_presence_of :year, :total
+  validates_presence_of :year, :total, :paid
   validates_numericality_of :year, only_integer: true, greater_than_or_equal_to: 2022
   validates_uniqueness_of :year, { scope: :thaali_id }
 
   validates_numericality_of :total, only_integer: true, greater_than: 0
+
+  validates_numericality_of :paid, only_integer: true, greater_than_or_equal_to: 0
 
 end
--- spec/models/takhmeen_spec.rb
+++ spec/models/takhmeen_spec.rb
@@ -24,5 +24,18 @@ RSpec.describe Takhmeen, type: :model do
 
             it { should validate_numericality_of(:total).is_greater_than(0) }
         end
+
+        context "paid" do
+            it { should validate_presence_of(:paid) }
+
+            it { should validate_numericality_of(:paid).only_integer }
+
+            it { should validate_numericality_of(:paid).is_greater_than_or_equal_to(0) }
+
+            it "must set its value to 0 after instance is persisted" do
+                subject.save
+                expect(subject.paid).to be_eql(0)
+            end
+        end
     end
 end
