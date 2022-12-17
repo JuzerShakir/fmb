6222be2c3803f0d5f219aeffc17523b1b5f8e147
Juzer Shakir <juzershakir.webdev@gmail.com>
2022-12-17 17:09:48 +0530

    pass the test suite for on_date transaction

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,7 +1,7 @@
 class Transaction < ApplicationRecord
   belongs_to :takhmeen
 
-  validates_presence_of :mode, :amount
+  validates_presence_of :mode, :amount, :on_date
 
   mode_of_payments = { cash: 0, cheque: 1, bank: 2 }
   enum :mode, mode_of_payments
