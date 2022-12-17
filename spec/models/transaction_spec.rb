01793629dcfc6beef1babe76a80ce8b1075e1473
Juzer Shakir <juzershakir.webdev@gmail.com>
2022-12-17 16:49:51 +0530

    create validation test suite

--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -2,6 +2,16 @@ require "rails_helper"
 
 RSpec.describe Transaction, type: :model do
     context "validation of attribute" do
+        context "mode" do
+            let(:mode_of_payments) { %i(cash cheque bank) }
+            it { should validate_presence_of(:mode) }
+            it { should define_enum_for(:mode).with_values(mode_of_payments) }
+        end
 
+        context "amount" do
+            it { should validate_presence_of(:amount) }
+            it { should validate_numericality_of(:amount).only_integer }
+            it { should validate_numericality_of(:amount).is_greater_than(0) }
+        end
     end
 end
