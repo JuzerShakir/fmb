f7cff0c3f0ac7cdf7fd0350e0e9060c1fee68539
Juzer Shakir <juzershakir.webdev@gmail.com>
2022-12-17 09:55:19 +0530

    factor out attribute word into a single test suit title & give singular association title

--- spec/models/thaali_spec.rb
+++ spec/models/thaali_spec.rb
@@ -1,23 +1,23 @@
 require "rails_helper"
 
 RSpec.describe Thaali, type: :model do
-    context "validations of" do
+    context "validations of attribute" do
         subject { build(:thaali) }
 
-        context "number attribute" do
+        context "number" do
             it { should validate_numericality_of(:number).only_integer }
             it { should validate_uniqueness_of(:number) }
             it { should validate_presence_of(:number) }
             it { should validate_numericality_of(:number).is_greater_than(0) }
         end
 
-        context "size attribute" do
+        context "size" do
             it { should define_enum_for(:size).with_values([:small, :medium, :large]) }
             it { should validate_presence_of(:size) }
         end
     end
 
-    context "associations" do
+    context "association" do
         it { should belong_to(:sabeel) }
     end
 
