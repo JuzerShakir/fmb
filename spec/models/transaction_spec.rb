46cd5dd8f7846ecf41815adf320dc943d07dce03
Juzer Shakir <juzershakir.webdev@gmail.com>
2022-12-17 17:09:04 +0530

    create test suite for on_date attrb

--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -13,5 +13,13 @@ RSpec.describe Transaction, type: :model do
             it { should validate_numericality_of(:amount).only_integer }
             it { should validate_numericality_of(:amount).is_greater_than(0) }
         end
+
+        context "on_date" do
+            it { should validate_presence_of(:on_date) }
+
+            it "must have a valid date" do
+                expect(subject.on_date).to be_an_instance_of(Date)
+            end
+        end
     end
 end
