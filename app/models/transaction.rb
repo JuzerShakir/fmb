8a3a7a924bcc012fca09cd83001f457a1a75da19
Juzer Shakir <juzershakir.webdev@gmail.com>
2022-12-17 16:50:07 +0530

    create passing validation test suite

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,3 +1,10 @@
 class Transaction < ApplicationRecord
   belongs_to :takhmeen
+
+  validates_presence_of :mode, :amount
+
+  mode_of_payments = { cash: 0, cheque: 1, bank: 2 }
+  enum :mode, mode_of_payments
+
+  validates_numericality_of :amount, only_integer: true, greater_than: 0
 end
