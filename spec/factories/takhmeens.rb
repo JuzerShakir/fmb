65c11d17b1d33c0bb0b026981492aae9b2acf802
Juzer Shakir <juzershakir.webdev@gmail.com>
2022-12-19 11:40:50 +0530

    generate takhmeen total with faker

--- spec/factories/takhmeens.rb
+++ spec/factories/takhmeens.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :takhmeen do
     thaali
     year { (Date.current.year..Date.current.next_year.year).to_a.sample }
-    sequence(:total, 40000)
+    total { Faker::Number.number(digits: 5) }
     balance { total - paid }
   end
 end
