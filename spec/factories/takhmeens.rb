d4d09d231724c68a4d1158e09598e1fc9df9679d
Juzer Shakir <juzershakir.webdev@gmail.com>
2022-12-19 11:34:33 +0530

    takhmeen year should generate year dynamically from this year to the next year

--- spec/factories/takhmeens.rb
+++ spec/factories/takhmeens.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :takhmeen do
     thaali
-    sequence(:year, 2022)
+    year { (Date.current.year..Date.current.next_year.year).to_a.sample }
     sequence(:total, 40000)
     balance { total - paid }
   end
