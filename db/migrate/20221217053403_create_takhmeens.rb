0f4d6626e477c0f788be9d1f1f9a812a4ea1ee5d
Juzer Shakir <juzershakir.webdev@gmail.com>
2022-12-21 11:21:45 +0530

    update takhmeen table with attributes from thaali_takhmeen

--- db/migrate/20221217053403_create_takhmeens.rb
+++ db/migrate/20221217053403_create_takhmeens.rb
@@ -1,16 +1,19 @@
-class CreateTakhmeens < ActiveRecord::Migration[7.0]
+class CreateThaaliTakhmeens < ActiveRecord::Migration[7.0]
   def change
-    create_table :takhmeens do |t|
-      t.references :thaali, null: false, foreign_key: true
-      t.integer :year, null: false, default: 2022
+    create_table :thaali_takhmeens do |t|
+      t.references :sabeel, null: false, foreign_key: true
+      t.integer :year, null: false, default: 2023
       t.integer :total, null: false
       t.integer :paid, null: false, default: 0
       t.integer :balance, null: false
       t.boolean :is_complete, null: false, default: false
+      t.integer :number, null: false
+      t.integer :size, null: false
 
       t.timestamps
     end
 
-    add_index :takhmeens, [:year, :thaali_id], unique: true
+    add_index :thaali_takhmeens, [:year, :sabeel_id], unique: true
+    add_index :thaali_takhmeens, [:year, :number], unique: true
   end
 end
