6829a95d31e0f93d0d6a1066d07035b9fea236b3
Juzer Shakir <juzershakir.webdev@gmail.com>
2022-12-21 10:24:45 +0530

    add test suite for paid attribute and have a passing test

--- app/models/thaali_takhmeen.rb
+++ app/models/thaali_takhmeen.rb
@@ -4,13 +4,14 @@ class ThaaliTakhmeen < ApplicationRecord
   # * Validations
   # number
   validates_numericality_of :number, :total, only_integer: true, greater_than: 0
-  validates_presence_of :number, :size, :year, :total
+  validates_presence_of :number, :size, :year, :total, :paid
   validates_uniqueness_of :number, { scope: :year}
   #sabeel_id
   validates_uniqueness_of :sabeel_id, { scope: :year}
   #year
   validates_numericality_of :year, only_integer: true, greater_than_or_equal_to: Date.today.year
-
+  #paid
+  validates_numericality_of :paid, only_integer: true, greater_than_or_equal_to: 0
 
   # * Enums
   enum :size, { small: 0, medium: 1, large: 2 }
--- spec/models/thaali_takhmeen_spec.rb
+++ spec/models/thaali_takhmeen_spec.rb
@@ -40,5 +40,14 @@ RSpec.describe ThaaliTakhmeen, type: :model do
             it { should validate_numericality_of(:total).only_integer }
             it { should validate_numericality_of(:total).is_greater_than(0) }
         end
+
+        context "paid" do
+            it { should validate_presence_of(:paid) }
+            it { should validate_numericality_of(:paid).only_integer }
+            it { should validate_numericality_of(:paid).is_greater_than_or_equal_to(0) }
+            it "is set to 0 by default after instance is instantiated" do
+                expect(subject.paid).to be_eql(0)
+            end
+        end
     end
 end
