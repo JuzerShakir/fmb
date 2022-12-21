b671af6687cda5f3e9eaa6450a5aa15c3e61bac4
Juzer Shakir <juzershakir.webdev@gmail.com>
2022-12-21 11:25:34 +0530

    update foreign key attribute to new table name

--- db/migrate/20221217105012_create_transactions.rb
+++ db/migrate/20221217105012_create_transactions.rb
@@ -1,7 +1,7 @@
 class CreateTransactions < ActiveRecord::Migration[7.0]
   def change
     create_table :transactions do |t|
-      t.references :takhmeen, null: false, foreign_key: true
+      t.references :thaali_takhmeen, null: false, foreign_key: true
       t.integer :mode, null: false
       t.integer :amount, null: false
       t.date :on_date, null: false
