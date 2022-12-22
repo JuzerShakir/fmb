7fb50c85ef5d15d11c3d27a8005bf8c53c575204
Juzer Shakir <juzershakir.webdev@gmail.com>
2022-12-22 21:55:42 +0530

    add passing test suite for create action in thaalis controller

--- app/controllers/thaali_takhmeens_controller.rb
+++ app/controllers/thaali_takhmeens_controller.rb
@@ -2,4 +2,16 @@ class ThaaliTakhmeensController < ApplicationController
     def new
         @sabeel = Sabeel.find(params[:id])
     end
+
+    def create
+        @thaali_takhmeen = ThaaliTakhmeen.new(thaali_takhmeen_params)
+        @thaali_takhmeen.save
+        redirect_to sabeel_thaali_takhmeen_path(@thaali_takhmeen)
+    end
+
+    private
+
+        def thaali_takhmeen_params
+            params.require(:thaali_takhmeen).permit(:number, :size, :sabeel_id, :total, :year)
+        end
 end
--- spec/requests/thaali_takhmeens_spec.rb
+++ spec/requests/thaali_takhmeens_spec.rb
@@ -1,10 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe "ThaaliTakhmeens", type: :request do
+  sabeel = FactoryBot.create(:sabeel)
+  valid_attributes = { number: 1, size: "small", year: 2022, total: 10000, sabeel_id: sabeel.id }
+  invalid_attributes = { number: nil, size: "medium", year: 2022, total: 20000, sabeel_id: sabeel.id }
+
   # * NEW
   context "GET new" do
     before do
-      sabeel = FactoryBot.create(:sabeel)
       get new_sabeel_thaali_takhmeen_path, params: { id: sabeel.id }
     end
 
@@ -14,5 +17,22 @@ RSpec.describe "ThaaliTakhmeens", type: :request do
     end
 
     it { should render_template(:new) }
+  end
+
+  context "POST create" do
+    context "with valid attributes" do
+      before do
+        post sabeel_thaali_takhmeens_path, params: { thaali_takhmeen: valid_attributes }
+      end
+
+      it "should create a new Thaali" do
+        expect(ThaaliTakhmeen.count).to eq 1
+      end
+
+      it "should redirect to created thaali" do
+        expect(response).to have_http_status(:found)
+        expect(response).to redirect_to sabeel_thaali_takhmeen_path(id: ThaaliTakhmeen.last.id)
+      end
+    end
   end
 end
