c866c80c5fbdf2d31e70cade5329c04393a12da6
Juzer Shakir <juzershakir.webdev@gmail.com>
2022-12-22 10:39:29 +0530

    setup test suite for new action and pass it

--- app/controllers/sabeels_controller.rb
+++ app/controllers/sabeels_controller.rb
@@ -1,2 +1,4 @@
 class SabeelsController < ApplicationController
+    def new
+    end
 end
--- spec/requests/sabeels_spec.rb
+++ spec/requests/sabeels_spec.rb
@@ -1,7 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe "Sabeels", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  context "GET new" do
+    before { get new_sabeel_path }
+
+    it "works" do
+      expect(response).to have_http_status(:ok)
+    end
   end
 end
