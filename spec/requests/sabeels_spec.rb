decb3cac2471110d8dd4e0e6515f0e4474120aee
Juzer Shakir <juzershakir.webdev@gmail.com>
2022-12-22 12:53:42 +0530

    add new test suite for new action

--- spec/requests/sabeels_spec.rb
+++ spec/requests/sabeels_spec.rb
@@ -1,11 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe "Sabeels", type: :request do
+
   context "GET new" do
     before { get new_sabeel_path }
 
     it "works" do
       expect(response).to have_http_status(:ok)
     end
+
+    it { should render_template(:new) }
   end
 end
