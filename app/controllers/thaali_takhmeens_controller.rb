52390bd2569b87dcc25ae6efc12618e57b916c1c
Juzer Shakir <juzershakir.webdev@gmail.com>
2022-12-23 11:24:27 +0530

    load previuos year thaali attributes to the new form if it exists

--- app/controllers/thaali_takhmeens_controller.rb
+++ app/controllers/thaali_takhmeens_controller.rb
@@ -3,6 +3,7 @@ class ThaaliTakhmeensController < ApplicationController
 
     def new
         @sabeel = Sabeel.find(params[:id])
+        @prev_thaali = sabeel.thaali_takhmeens.where(year: $PREV_YEAR_TAKHMEEN)
     end
 
     def create
