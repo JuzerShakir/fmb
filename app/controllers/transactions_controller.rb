555976f6a9c433951f6835ac64d219f61a81baec
Juzer Shakir <juzershakir.webdev@gmail.com>
2022-12-24 12:43:48 +0530

    create passing test suite for create action of transaction controller

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -2,4 +2,19 @@ class TransactionsController < ApplicationController
     def new
         @thaali_takhmeen = ThaaliTakhmeen.find(params[:id])
     end
+
+    def create
+        @transaction = Transaction.new(transaction_params)
+        if @transaction.valid?
+            @transaction.save
+            redirect_to thaali_takhmeen_path
+        else
+            render :new
+        end
+    end
+
+    private
+        def transaction_params
+            params.require(:transaction).permit(:amount, :on_date, :mode, :thaali_takhmeen_id)
+        end
 end
--- spec/requests/transactions_spec.rb
+++ spec/requests/transactions_spec.rb
@@ -13,4 +13,45 @@ RSpec.describe "Transactions", type: :request do
             expect(response).to render_template(:new)
         end
     end
+
+    # * CREATE
+    context "GET create" do
+        before do
+            @thaali = FactoryBot.create(:thaali_takhmeen)
+        end
+
+        context "with valid attributes" do
+            before do
+                @valid_attributes = FactoryBot.attributes_for(:transaction, thaali_takhmeen_id: @thaali.id)
+                post "/thaali_takhmeens/#{@thaali.id}/transactions", params: { transaction: @valid_attributes }
+                @transaction = Transaction.find_by(amount: @valid_attributes[:amount])
+            end
+
+            it "should create a new Transaction" do
+                expect(@transaction).to be_truthy
+            end
+
+            it "should redirect to created thaali" do
+              expect(response).to have_http_status(:found)
+              expect(response).to redirect_to thaali_takhmeen_path
+            end
+        end
+
+        context "with invalid attributes" do
+            before do
+                @invalid_attributes = FactoryBot.attributes_for(:transaction, amount: nil)
+                post "/thaali_takhmeens/#{@thaali.id}/transactions", params: { transaction: @invalid_attributes }
+                @transaction = Transaction.find_by(on_date: @invalid_attributes[:on_date])
+            end
+
+            it "does not create a new Thaali" do
+                expect(@transaction).to be_nil
+            end
+
+            it "should render a new template" do
+                expect(response).to render_template(:new)
+                expect(response).to have_http_status(:ok)
+            end
+      end
+    end
 end
