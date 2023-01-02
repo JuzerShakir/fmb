b5358a44e51e20df3d236cfad0416fc06b2014f4
Juzer Shakir <juzershakir.webdev@gmail.com>
2023-01-02 17:38:20 +0530

    implement debouncing of 650ms for thaali_takhmeen search

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -2,10 +2,13 @@
 // Run that command whenever you add a new controller or create them with
 // ./bin/rails generate stimulus controllerName
 
-import { application } from "./application"
+import { application } from "./application";
 
-import AutohideController from "./autohide_controller"
-application.register("autohide", AutohideController)
+import AutohideController from "./autohide_controller";
+application.register("autohide", AutohideController);
 
-import HelloController from "./hello_controller"
-application.register("hello", HelloController)
+import HelloController from "./hello_controller";
+application.register("hello", HelloController);
+
+import SearchController from "./search_controller";
+application.register("search", SearchController);
