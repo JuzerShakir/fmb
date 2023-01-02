ab4805b8ab176da5e70ae732cc496e9122d15d05
Juzer Shakir <juzershakir.webdev@gmail.com>
2023-01-02 23:36:52 +0530

    lowercase Global Variable of phases

--- spec/factories/sabeels.rb
+++ spec/factories/sabeels.rb
@@ -1,28 +1,28 @@
 # * Global variables
-$PHASE_1 = %w(mohammedi saifee jamali taiyebi imadi burhani zaini fakhri badri ezzi)
-$PHASE_2 = %w(maimoon_a maimoon_b qutbi_a qutbi_b najmi)
-$PHASE_3 = %w(husami_a husami_b noorani_a noorani_b)
+$phase_1 = %w(mohammedi saifee jamali taiyebi imadi burhani zaini fakhri badri ezzi)
+$phase_2 = %w(maimoon_a maimoon_b qutbi_a qutbi_b najmi)
+$phase_3 = %w(husami_a husami_b noorani_a noorani_b)
 
 FactoryBot.define do
 
   factory :sabeel do
     its { Faker::Number.number(digits: 8) }
     hof_name { Faker::Name.name }
-    apartment { Array.new.push(*$PHASE_1, *$PHASE_2, *$PHASE_3).sample }
+    apartment { Array.new.push(*$phase_1, *$phase_2, *$phase_3).sample }
     flat_no { Faker::Number.within(range: 1..9999) }
     mobile { Faker::Number.number(digits: 10) }
     email { Faker::Internet.free_email }
 
     trait :in_phase_1 do
-      apartment { $PHASE_1.sample }
+      apartment { $phase_1.sample }
     end
 
     trait :in_phase_2 do
-      apartment { $PHASE_2.sample }
+      apartment { $phase_2.sample }
     end
 
     trait :in_phase_3 do
-      apartment { $PHASE_3.sample }
+      apartment { $phase_3.sample }
     end
 
     # factory :sabeel_with_thaali, traits: [:with_thaali]
