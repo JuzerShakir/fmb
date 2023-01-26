c45a9efbdd9548fff07f403e8de227d297031256
Juzer Shakir <juzershakir.webdev@gmail.com>
2023-01-27 04:59:08 +0530

    create passing test suite for 'its' attribute of user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ApplicationRecord
     has_secure_password
+
+    # ITS
+    validates_presence_of :its, :name, message: "cannot be blank"
+    validates_numericality_of :its, only_integer: true, message: "must be a number"
+    validates_numericality_of :its, in: 10000000..99999999, message: "is invalid"
+    validates_uniqueness_of :its, message: "has already been registered"
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,7 @@
 require "rails_helper"
 
 RSpec.describe User, type: :model do
-    # subject { build(:user) }
+    subject { build(:user) }
 
     context "validations of attribute" do
         context "ITS" do
