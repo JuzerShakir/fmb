0c163170ee2b3c0ea7b7303f34b33d37b39ce109
Juzer Shakir <juzershakir.webdev@gmail.com>
2023-01-27 10:25:08 +0530

    create passing test suite for testing 'create' action of User controller

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -12,6 +12,46 @@ RSpec.describe "Users", type: :request do
         end
     end
 
+    # * CREATE
+    context "POST create" do
+        before do
+            @valid =  FactoryBot.attributes_for(:user)
+            @invalid = FactoryBot.attributes_for(:invalid_user)
+        end
+
+        context "with valid attributes" do
+            before do
+                post users_path, params: { user: @valid }
+                @user = User.find_by(its: @valid[:its])
+            end
+
+            it "should create a new User" do
+                expect(@user).to be_truthy
+            end
+
+            it "should redirect to index path of User" do
+                expect(response).to have_http_status(:found)
+                expect(response).to redirect_to admin_path
+            end
+        end
+
+        context "with invalid attributes" do
+            before do
+                post users_path, params: { user: @invalid }
+                @invalid_user = User.find_by(its: @invalid[:its])
+            end
+
+            it "does not create a new User" do
+                expect(@invalid_user).to be_nil
+            end
+
+            it "should render a new template" do
+                expect(response).to render_template(:new)
+                expect(response).to have_http_status(:unprocessable_entity)
+            end
+        end
+    end
+
     # * INDEX
     context "GET index" do
         before { get admin_path }
