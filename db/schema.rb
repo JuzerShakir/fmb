c60816870051eaaaed762e1f0059cc2de1cd3c62
Juzer Shakir <juzershakir.webdev@gmail.com>
2023-01-27 11:44:54 +0530

    add slug for 'its' attribute of User model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_26_231421) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_27_060140) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -79,7 +79,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_26_231421) do
     t.string "password_digest", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "slug"
     t.index ["its"], name: "index_users_on_its", unique: true
+    t.index ["slug"], name: "index_users_on_slug", unique: true
   end
 
   add_foreign_key "thaali_takhmeens", "sabeels"
