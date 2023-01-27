3e7cb8c3c1a88ff393d6514f5fa2d98140aa42ac
Juzer Shakir <juzershakir.webdev@gmail.com>
2023-01-27 06:29:10 +0530

    replace 'password_digest' attrb with 'password' & 'password_confirmation'

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,6 +2,7 @@ FactoryBot.define do
   factory :user do
     its { Faker::Number.number(digits: 8) }
     name { Faker::Name.name }
-    password_digest { Faker::Internet.password(min_length: 3, max_length: 35) }
+    password { Faker::Internet.password(min_length: 3, max_length: 35) }
+    password_confirmation { password }
   end
 end
