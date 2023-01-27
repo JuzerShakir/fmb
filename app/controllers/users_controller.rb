9b9e7293bec3e3274786d5b55a1bc14e7e4bdc4d
Juzer Shakir <juzershakir.webdev@gmail.com>
2023-01-27 06:30:51 +0530

    create passing test suite for valid values of user of 'create' action of User controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,22 @@
 class UsersController < ApplicationController
 
     def new
+        @user = User.new
     end
+
+    def create
+        @user = User.new(user_params)
+        if @user.valid?
+            @user.save
+            redirect_to root_path, success: "User created successfully"
+        else
+            render :new, status: :unprocessable_entity
+        end
+    end
+
+    private
+
+        def user_params
+            params.require(:user).permit(:its, :name, :password, :password_confirmation)
+        end
 end
