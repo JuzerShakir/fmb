ff36ce00381e23e055876b98bee39c440f88801e
Juzer Shakir <juzershakir.webdev@gmail.com>
2023-01-27 10:24:31 +0530

    fix minimum & maximum length of 'password' of User

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     its { Faker::Number.number(digits: 8) }
     name { Faker::Name.name }
-    password { Faker::Internet.password(min_length: 3, max_length: 35) }
+    password { Faker::Internet.password(min_length: 6, max_length: 72) }
     password_confirmation { password }
 
 
