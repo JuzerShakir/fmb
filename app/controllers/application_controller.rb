36dc28f94c904732919219ec80f1de5694cec238
Juzer Shakir <juzershakir.webdev@gmail.com>
2023-01-28 10:39:54 +0530

    create current_user method for controller and helper class to check if user is logged in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,12 @@
 class ApplicationController < ActionController::Base
     include Pagy::Backend
     add_flash_types :success, :notice
+
+    private
+
+        def current_user
+            @current_user ||= User.find(session[:user_id]) if session[:user_id]
+        end
+
+        helper_method :current_user
 end
