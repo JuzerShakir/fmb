22a9ea6c0d53c28ee4594300e2eff5395a00db2a
Juzer Shakir <juzershakir.webdev@gmail.com>
2023-01-29 00:38:10 +0530

    dynamically set values of 'mode' attribute of transaction

--- spec/factories/transactions.rb
+++ spec/factories/transactions.rb
@@ -1,9 +1,7 @@
 FactoryBot.define do
-  mode_of_payments = %i(cash cheque bank)
-
   factory :transaction do
     thaali_takhmeen
-    sequence :mode, mode_of_payments.cycle
+    sequence :mode, Transaction.modes.keys.cycle
     amount { Faker::Number.number(digits: 4) }
     on_date { Faker::Date.backward }
     recipe_no { Random.rand(1000..100000) }
