f6edc290d3c99eb41d71f840956db0c45c76b420
Juzer Shakir <juzershakir.webdev@gmail.com>
2023-01-28 19:18:45 +0530

    refactor setting user logic to a before action in User controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
     before_action :authorize_user, only: [:show, :edit, :update, :destroy]
+    before_action :set_user, only: [:show, :edit, :update, :destroy]
 
     def index
         @users = User.all.where.not(id: current_user.id)
@@ -20,15 +21,12 @@ class UsersController < ApplicationController
     end
 
     def show
-        @user = User.find(params[:id])
     end
 
     def edit
-        @user = User.find(params[:id])
     end
 
     def update
-        @user = User.find(params[:id])
         if @user.update(user_params)
             redirect_to @user, success: "User updated successfully"
         else
@@ -37,7 +35,6 @@ class UsersController < ApplicationController
     end
 
     def destroy
-        @user = User.find(params[:id])
         @user.destroy
         session[:user_id] = nil
         #  if admin deletes other user
@@ -50,5 +47,9 @@ class UsersController < ApplicationController
 
         def user_params
             params.require(:user).permit(:its, :name, :password, :password_confirmation)
+        end
+
+        def set_user
+            @user = User.find(params[:id])
         end
 end
