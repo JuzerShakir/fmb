38b5bb6d7ab7c789d6604ff6f941467e2adde443
Juzer Shakir <juzershakir.webdev@gmail.com>
2023-01-30 21:11:55 +0530

    create footer view with passing test suite

---
+++ spec/features/footer_spec.rb
@@ -0,0 +1,46 @@
+require 'rails_helper'
+
+RSpec.describe "Footer features" do
+    context "should have a footer showing - ", js: true do
+        before do
+            visit login_path
+        end
+
+        scenario "fontawesome '©' logo" do
+            within "footer" do
+                expect(page).to have_css(".fa-copyright")
+            end
+        end
+
+        scenario "dynammic current year" do
+            within "footer" do
+                expect(page).to have_content("#{Date.current.year}")
+            end
+        end
+
+        scenario "role of the creator" do
+            within "footer" do
+                expect(page).to have_content("Developed & Designed")
+            end
+        end
+
+        scenario "fontawesome '❤' logo" do
+            within "footer" do
+                expect(page).to have_css(".fa-heart")
+            end
+        end
+
+        scenario "creator name linking to its portfolio site" do
+            within "footer" do
+                expect(page).to have_link("Juzer Shakir")
+                @window = window_opened_by { click_on  "Juzer Shakir" }
+
+                using_wait_time 2 do
+                    within_window @window do
+                        expect(page).to have_current_path("https://juzershakir.github.io/")
+                    end
+                end
+            end
+        end
+    end
+end
