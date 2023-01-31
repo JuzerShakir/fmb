f3f6cf2426b2cfe224caba8a8013b067885325c3
Juzer Shakir <juzershakir.webdev@gmail.com>
2023-01-31 15:58:34 +0530

    add new factories for each role

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -6,11 +6,26 @@ FactoryBot.define do
     password { Faker::Internet.password(min_length: 6, max_length: 72) }
     password_confirmation { password }
 
-
     trait :wrong_password do
       password_confirmation { nil }
     end
 
+    trait :admin do
+      role { "admin" }
+    end
+
+    trait :member do
+      role { "member" }
+    end
+
+    trait :viewer do
+      role { "viewer" }
+    end
+
     factory :invalid_user, traits: [:wrong_password]
+    factory :admin_user, traits: [:admin]
+    factory :member_user, traits: [:member]
+    factory :viewer_user, traits: [:viewer]
+
   end
 end
