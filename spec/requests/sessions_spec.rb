a0a7818ef68d620951f1a6a444a78e417c2a0b57
Juzer Shakir <juzershakir.webdev@gmail.com>
2023-02-04 15:13:47 +0530

    a logged in user cannot access the 'new' action of sessions controller

--- spec/requests/sessions_spec.rb
+++ spec/requests/sessions_spec.rb
@@ -1,14 +1,37 @@
 require 'rails_helper'
 
-RSpec.describe "Sessions", type: :request do
+RSpec.describe "Session request - session value is 👉", type: :request do
+    before do
+        @password = Faker::Internet.password(min_length: 6, max_length: 72)
+    end
 
-    # * NEW
-    context "GET new" do
-        before { get login_path }
+    # * NULL
+    context "'null' CAN access" do
+        # * NEW
+        context "GET new" do
+            before { get login_path }
 
-        it "should render a new template with 200 status code" do
-            expect(response).to render_template(:new)
-            expect(response).to have_http_status(:ok)
+            it "should render a new template with 200 status code" do
+                expect(response).to render_template(:new)
+                expect(response).to have_http_status(:ok)
+            end
+        end
+    end
+
+    # * NOT NULL
+    context "NOT 'null' CANNOT access" do
+        # * NEW
+        context "GET new" do
+            before do
+                @user = FactoryBot.create(:user, password: @password)
+                post signup_path, params: { sessions: @user.attributes.merge({ password: @password }) }
+                get login_path
+            end
+
+            it "should redirect to root path with 302 status code" do
+                expect(response).to redirect_to root_path
+                expect(response).to have_http_status(:found)
+            end
         end
     end
 end
