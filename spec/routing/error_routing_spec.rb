d3bd4993980af5911c046798bbd1dbfdbdd9c6c1
Juzer Shakir <juzershakir.webdev@gmail.com>
2023-02-13 11:33:23 +0530

    edit test suite titles for better readability

--- spec/routing/error_routing_spec.rb
+++ spec/routing/error_routing_spec.rb
@@ -1,14 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe "Error", type: :routing do
-    context "404 (Not Found) route" do
-        it "ErrorsController" do
+    context "404 (Not Found) if user visits an unkonwn link" do
+        it "redirects to 'not_found' action" do
             expect(get("/404")).to route_to("errors#not_found")
         end
     end
 
-    context "500 (Internal Server) route" do
-        it "ErrorsController" do
+    context "500 (Internal Server) if server encountered an unexpected condition" do
+        it "redirects to 'internal_server' action" do
             expect(get("/500")).to route_to("errors#internal_server")
         end
     end
