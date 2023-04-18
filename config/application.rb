048e17464764960670c56e61609e338675eb98e0
Juzer Shakir <juzershakir.webdev@gmail.com>
2023-04-19 01:17:46 +0530

    add timezone to the application

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,7 @@ module Fmb
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
     config.exceptions_app = self.routes
+    config.time_zone = "Asia/Kolkata"
 
     # Configuration for the application, engines, and railties goes here.
     #
