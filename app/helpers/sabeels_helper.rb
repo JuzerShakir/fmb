255bab3722cb9e44426dc44992e86faf0f8a7fbd
Juzer Shakir <juzershakir.webdev@gmail.com>
2023-04-19 12:26:39 +0530

    replace hint message for 'its' & 'mobile' attribute from helper method to translation

--- app/helpers/sabeels_helper.rb
+++ app/helpers/sabeels_helper.rb
@@ -1,9 +1,2 @@
 module SabeelsHelper
-    def its_message
-        "must be exactly 8 characters long"
-    end
-
-    def mobile_message
-        "without the country code"
-    end
 end
--- spec/helpers/sabeels_helper_spec.rb
+++ spec/helpers/sabeels_helper_spec.rb
@@ -1,15 +1,4 @@
 require 'rails_helper'
 
 RSpec.describe SabeelsHelper, type: :helper do
-    describe "#its_message" do
-        it "returns a hint about no. of chars allowed" do
-          expect(helper.its_message).to eq("must be exactly 8 characters long")
-        end
-    end
-
-    describe "#mobile_message" do
-        it "returns a hint to not include country code" do
-          expect(helper.mobile_message).to eq("without the country code")
-        end
-    end
 end
