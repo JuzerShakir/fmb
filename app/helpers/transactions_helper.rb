0dd4a8708dbea4bd5a771819383ee696b0cd3dd4
Juzer Shakir <juzershakir.webdev@gmail.com>
2023-04-19 12:29:39 +0530

    hint message of 'year' attribute from local variable to a helper method

--- app/helpers/transactions_helper.rb
+++ app/helpers/transactions_helper.rb
@@ -2,4 +2,8 @@ module TransactionsHelper
   def readable_on_date(date)
    date.to_time.strftime('%A, %b %d %Y')
   end
+
+  def amount_message
+    "Amount shouldn't be greater than: #{@total_balance}"
+  end
 end
