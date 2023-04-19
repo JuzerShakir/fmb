a84659f6ddaf82ffaff42df628c8209f3ef44bb9
Juzer Shakir <juzershakir.webdev@gmail.com>
2023-04-19 13:48:53 +0530

    replace hint messages of 'name', 'password' & 'password_confirmation' attributes of User model from helper method to translation file

--- spec/helpers/users_helper_spec.rb
+++ spec/helpers/users_helper_spec.rb
@@ -1,21 +1,4 @@
 require 'rails_helper'
 
 RSpec.describe UsersHelper, type: :helper do
-    describe "#its_message" do
-        it "returns a hint about no. of chars allowed" do
-            expect(helper.its_message).to eq("must be exactly 8 characters long")
-        end
-    end
-
-     describe "#name_message" do
-        it "returns a hint about the length of the name allowed" do
-          expect(helper.name_message).to eq("between 3 to 35 characters long")
-        end
-     end
-
-     describe "#password_message" do
-        it "returns a hint about the length of the password allowed" do
-          expect(helper.password_message).to eq("between 6 to 72 characters long")
-        end
-     end
 end
