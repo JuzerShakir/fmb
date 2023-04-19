57ad21ec6f6f32ba350c8e58b11fa86c2484f336
Juzer Shakir <juzershakir.webdev@gmail.com>
2023-04-19 13:44:50 +0530

    show 'its' attribute name in an uppercase form

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,13 +1,2 @@
 module UsersHelper
-    def its_message
-        "must be exactly 8 characters long"
-    end
-
-    def name_message
-        "between 3 to 35 characters long"
-    end
-
-    def password_message
-        "between 6 to 72 characters long"
-    end
 end
