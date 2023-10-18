a33179a6ac6d4988c4aaa1b84a54228008bacfb3
Juzer Shakir <juzershakir.webdev@gmail.com>
2023-10-18 13:40:29 +0530

    fix all rubocop errors in models/transaction_spec

--- spec/factories/transactions.rb
+++ spec/factories/transactions.rb
@@ -7,5 +7,16 @@ FactoryBot.define do
     amount { Faker::Number.number(digits: 4) }
     date { Faker::Date.backward }
     recipe_no { Random.rand(1000..100000) }
+
+    trait :today do
+      date { Time.zone.now.to_date }
+    end
+
+    trait :yesterday do
+      date { Time.zone.now.to_date.yesterday }
+    end
+
+    factory :today_transactions, traits: [:today]
+    factory :yesterday_transactions, traits: [:yesterday]
   end
 end
--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -3,126 +3,100 @@
 require "rails_helper"
 
 RSpec.describe Transaction do
-  subject { build(:transaction) }
+  subject(:transaction) { build(:transaction) }
 
-  today = Time.zone.now.to_date
-  yesterday = today.prev_day
-
-  context "assocaition" do
+  context "with assocaition" do
     subject { create(:transaction) }
 
     it { is_expected.to belong_to(:thaali_takhmeen) }
   end
 
-  context "validation of attribute" do
-    context "mode" do
+  context "when validating" do
+    context "with mode" do
       let(:mode_of_payments) { %i[cash cheque bank] }
 
       it { is_expected.to validate_presence_of(:mode).with_message("must be selected") }
       it { is_expected.to define_enum_for(:mode).with_values(mode_of_payments) }
     end
 
-    context "amount" do
+    context "with amount" do
       it { is_expected.to validate_numericality_of(:amount).only_integer.with_message("must be a number") }
       it { is_expected.to validate_numericality_of(:amount).is_greater_than(0).with_message("must be greater than 0") }
+      it { expect(transaction.amount).to be_present }
+
+      it "will raise an error if amount is greater than balance" do
+        transaction.amount = transaction.thaali_takhmeen.balance + Faker::Number.non_zero_digit
+        transaction.validate
+        expect(transaction.errors[:amount]).to include("cannot be greater than the balance")
+      end
+
+      it "will NOT raise an error if amount is less than or equal to balance" do
+        transaction.amount = transaction.thaali_takhmeen.balance - Faker::Number.non_zero_digit
+        transaction.validate
+        expect(transaction.errors[:amount]).not_to include("cannot be greater than the balance")
+      end
     end
 
-    context "date" do
-      let(:today_str) { Date.current.strftime("%e %B %Y") }
+    context "with date" do
+      today_str = Date.current.strftime("%e %B %Y")
 
       it { is_expected.to validate_presence_of(:date).with_message("must be selected") }
 
-      it "must have a valid date" do
-        expect(subject.date).to be_an_instance_of(Date)
-      end
+      it { expect(transaction.date).to be_an_instance_of(Date) }
 
-      it "is less than or equal to the current date" do
-        subject.date = today
-        subject.validate
-        expect(subject.errors[:date]).not_to include("must be on or before #{today_str}")
+      describe "will NOT raise an error if it's less than or equal to the current date" do
+        subject { create(:today_transactions).errors[:date] }
+
+        it { is_expected.not_to include("must be on or before #{today_str}") }
       end
 
       it "raises error for future dates" do
-        subject.date = Faker::Date.forward
-        subject.validate
-        expect(subject.errors[:date]).to include("must be on or before #{today_str}")
+        transaction.date = Faker::Date.forward
+        transaction.validate
+        expect(transaction.errors[:date]).to include("must be on or before #{today_str}")
       end
     end
 
-    context "recipe_no" do
+    context "with recipe_no" do
       it { is_expected.to validate_numericality_of(:recipe_no).only_integer.with_message("must be a number") }
       it { is_expected.to validate_numericality_of(:recipe_no).is_greater_than(0).with_message("must be greater than 0") }
       it { is_expected.to validate_uniqueness_of(:recipe_no).with_message("has already been registered") }
     end
   end
 
-  context "custom validation method" do
-    describe "#amount_should_be_less_than_the_balance" do
-      it "amount value should not be nil" do
-        expect(subject.amount).to be_present
-      end
-
-      context "must raise an error" do
-        it "for new transactions" do
-          subject.amount = subject.thaali_takhmeen.balance + Faker::Number.non_zero_digit
-          subject.validate
-          expect(subject.errors[:amount]).to include("cannot be greater than the balance")
-        end
-
-        it "for editing existing transaction" do
-          takhmeen = create(:thaali_takhmeen)
-          trans = create(:transaction, thaali_takhmeen_id: takhmeen.id)
-
-          trans.amount += takhmeen.balance + Random.rand(1..10)
-          trans.validate
-          expect(trans.errors[:amount]).to include("cannot be greater than the balance")
-        end
-      end
-
-      it "must NOT raise an error" do
-        subject.amount = subject.thaali_takhmeen.balance - Faker::Number.non_zero_digit
-        subject.validate
-        expect(subject.errors[:amount]).not_to include("cannot be greater than the balance")
-      end
-    end
-  end
-
-  context "callback method" do
+  context "when saving" do
     describe "#add_all_transaction_amounts_to_paid_amount" do
+      let(:takhmeen) { transaction.thaali_takhmeen }
+      let(:all_transactions_of_a_takhmeen) { takhmeen.transactions }
+
       it { is_expected.to callback(:add_all_transaction_amounts_to_paid_amount).after(:commit) }
 
-      context "if takhmeen has ONE OR MORE transactions" do
-        subject { create(:transaction) }
+      it "adds all the transaction amounts if it has ONE OR MORE transactions" do
+        transaction.save
 
-        let!(:takhmeen) { subject.thaali_takhmeen }
-        let!(:all_transactions_of_a_takhmeen) { takhmeen.transactions }
-
-        it "adds all the transaction amounts" do
-          total_takhmeen = all_transactions_of_a_takhmeen.pluck(:amount).sum(0)
-          expect(takhmeen.paid).to eq(total_takhmeen)
-        end
+        total_takhmeen = all_transactions_of_a_takhmeen.pluck(:amount).sum(0)
+        expect(takhmeen.paid).to eq(total_takhmeen)
       end
 
-      context "if takhmeen has NO transactions" do
-        it "resets paid amount to zero" do
-          subject.destroy
-          expect(subject.thaali_takhmeen.paid).to eq(0)
-        end
+      it "paid amount is 0 if it has NO transactions" do
+        transaction.destroy
+        expect(transaction.thaali_takhmeen.paid).to eq(0)
       end
     end
   end
 
-  context "scope" do
-    describe ".that_occured_on" do
-      let!(:transaction_today) { create(:transaction, date: today) }
-      let!(:transaction_prev_day) { create(:transaction, date: yesterday) }
+  context "when using scope" do
+    describe "returns all transactions for the given date" do
+      let(:todays_transactions) { create(:today_transactions) }
+      let(:yesterdays_transactions) { create(:yesterday_transactions) }
+      let(:today) { Time.zone.now.to_date }
 
       it "returns all the transactions that occured on the given date" do
-        expect(described_class.that_occured_on(today)).to contain_exactly(transaction_today)
+        expect(described_class.that_occured_on(:today)).to contain_exactly(todays_transactions)
       end
 
       it "does not return the transactions that occured on some other day" do
-        expect(described_class.that_occured_on(today)).not_to contain_exactly(transaction_prev_day)
+        expect(described_class.that_occured_on(:today)).not_to contain_exactly(yesterdays_transactions)
       end
     end
   end
