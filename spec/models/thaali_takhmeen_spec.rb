bee3fb9802c705b9f42ba1475131f0e0364b65f6
Juzer Shakir <juzershakir.webdev@gmail.com>
2023-10-18 12:39:53 +0530

    fix all rubocop errors in models/thaali_takhmeen_spec

--- spec/models/thaali_takhmeen_spec.rb
+++ spec/models/thaali_takhmeen_spec.rb
@@ -3,59 +3,56 @@
 require "rails_helper"
 
 RSpec.describe ThaaliTakhmeen do
-  subject { build(:thaali_takhmeen) }
+  subject(:thaali) { build(:thaali_takhmeen) }
 
-  context "association" do
+  context "with association" do
     it { is_expected.to belong_to(:sabeel) }
     it { is_expected.to have_many(:transactions).dependent(:destroy) }
   end
 
-  context "validations of attribute" do
-    context "number" do
+  context "when validating" do
+    context "with number" do
       it { is_expected.to validate_numericality_of(:number).only_integer.with_message("must be a number") }
       it { is_expected.to validate_numericality_of(:number).is_greater_than(0).with_message("must be greater than 0") }
       it { is_expected.to validate_uniqueness_of(:number).scoped_to(:year).with_message("has already been taken for the selected year") }
     end
 
-    context "size" do
+    context "with size" do
       it { is_expected.to define_enum_for(:size).with_values([:small, :medium, :large]) }
       it { is_expected.to validate_presence_of(:size).with_message("cannot be blank") }
     end
 
-    context "year" do
+    context "with year" do
       it { is_expected.to validate_numericality_of(:year).only_integer.with_message("must be a number") }
       it { is_expected.to validate_numericality_of(:year).is_less_than_or_equal_to(CURR_YR).with_message("must be less than or equal to #{CURR_YR}") }
       it { is_expected.to validate_uniqueness_of(:year).scoped_to(:sabeel_id).with_message("sabeel is already taking thaali for selected year") }
     end
 
-    context "total" do
+    context "with total" do
       it { is_expected.to validate_numericality_of(:total).only_integer.with_message("must be a number") }
       it { is_expected.to validate_numericality_of(:total).is_greater_than(0).with_message("must be greater than 0") }
     end
 
-    context "paid" do
+    context "with paid" do
       it { is_expected.to validate_numericality_of(:paid).only_integer.with_message("must be a number") }
       it { is_expected.to validate_numericality_of(:paid).is_greater_than_or_equal_to(0) }
-
-      it "is set to 0 by default after instance is instantiated" do
-        expect(subject.paid).to be_eql(0)
-      end
+      it { expect(thaali.paid).to be_eql(0) }
     end
 
-    context "is_complete" do
+    context "with is_complete" do
       it "must set its value to false after instance is persisted" do
-        expect(subject.is_complete).to be_falsey
+        expect(thaali.is_complete).to be_falsey
       end
     end
   end
 
-  context "callback method" do
+  context "when saving" do
     describe "#update_balance" do
       it { is_expected.to callback(:update_balance).before(:save) }
 
       it "must instantiate balance attribute with same amount as total attribute amount" do
-        subject.save
-        expect(subject.balance).to eq(subject.total)
+        thaali.save
+        expect(thaali.balance).to eq(thaali.total)
       end
     end
 
@@ -63,14 +60,14 @@ RSpec.describe ThaaliTakhmeen do
       it { is_expected.to callback(:check_if_balance_is_zero).before(:save) }
 
       it "must set is_complete attribute to truthy" do
-        subject.paid = subject.total = Faker::Number.number(digits: 5)
-        subject.save
-        expect(subject.is_complete).to be_truthy
+        thaali.paid = thaali.total = Faker::Number.number(digits: 5)
+        thaali.save
+        expect(thaali.is_complete).to be_truthy
       end
     end
   end
 
-  context "scope" do
+  context "when using scope" do
     let!(:current_year_takhmeen) { create(:active_takhmeen) }
     let!(:previous_year_takhmeen) { create(:previous_takhmeen) }
 
