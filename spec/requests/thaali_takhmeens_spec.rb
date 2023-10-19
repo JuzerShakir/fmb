ed90916f74ff2a2854e9026871c75070bfb809ce
Juzer Shakir <juzershakir.webdev@gmail.com>
2023-10-19 16:51:58 +0530

    fix all rubocop errors in spec/requests/thaali_takhmeens

--- spec/requests/thaali_takhmeens_spec.rb
+++ spec/requests/thaali_takhmeens_spec.rb
@@ -2,222 +2,215 @@
 
 require "rails_helper"
 
-RSpec.describe "ThaaliTakhmeen request - user type 👉" do
-  before do
-    @password = Faker::Internet.password(min_length: 6, max_length: 72)
-  end
-
+RSpec.describe "ThaaliTakhmeen request" do
   # * Accessible by all
-  context "any user can access 👉" do
-    before do
-      @user = create(:user, password: @password)
-      post signup_path, params: {sessions: @user.attributes.merge({password: @password})}
+  describe "'Anyone' who have logged in can access 👉" do
+    # rubocop:disable RSpec/BeforeAfterAll
+    before(:all) do
+      user = create(:user)
+      post signup_path, params: {sessions: user.attributes.merge({password: user.password})}
     end
+    # rubocop:enable RSpec/BeforeAfterAll
 
     # * INDEX
-    context "GET index" do
-      before do
-        get root_path
+    describe "GET /index" do
+      before { get root_path }
+
+      it "renders index template" do
+        expect(response).to render_template(:index)
       end
 
-      it "renders an index template with 200 status code" do
+      it "returns with 200 status code" do
         expect(response).to have_http_status(:ok)
-        expect(response).to render_template(:index)
       end
     end
 
     # * SHOW
-    context "GET show" do
-      before do
-        @thaali = create(:thaali_takhmeen)
-        get takhmeen_path(@thaali.id)
-      end
+    describe "GET /show" do
+      let(:thaali) { create(:thaali_takhmeen) }
+
+      before { get takhmeen_path(thaali.id) }
 
       it "renders a show template" do
         expect(response).to render_template(:show)
+      end
+
+      it "returns with 200 status code" do
         expect(response).to have_http_status(:ok)
       end
 
       it "renders the instance that was passed in the params" do
         # it could be any attribute, not only number
-        expect(response.body).to include(@thaali.number.to_s)
+        expect(response.body).to include(thaali.number.to_s)
       end
     end
 
     # * STATISTICS
-    context "GET stats" do
-      before do
-        get takhmeens_stats_path
+    describe "GET /stats" do
+      before { get takhmeens_stats_path }
+
+      it "renders stats template" do
+        expect(response).to render_template(:stats)
       end
 
-      it "renders a stats template" do
-        expect(response).to render_template(:stats)
+      it "returns with 200 status code" do
         expect(response).to have_http_status(:ok)
       end
     end
 
     # * COMPLETE
-    context "GET complete" do
-      before do
-        get thaali_takhmeens_complete_path(CURR_YR)
+    describe "GET /complete" do
+      before { get thaali_takhmeens_complete_path(CURR_YR) }
+
+      it "renders complete template" do
+        expect(response).to render_template(:complete)
       end
 
-      it "renders a complete template" do
-        expect(response).to render_template(:complete)
+      it "returns with 200 status code" do
         expect(response).to have_http_status(:ok)
       end
     end
 
     # * PENDING
-    context "GET pending" do
-      before do
-        get thaali_takhmeens_pending_path(CURR_YR)
+    describe "GET /pending" do
+      before { get thaali_takhmeens_pending_path(CURR_YR) }
+
+      it "renders pending template" do
+        expect(response).to render_template(:pending)
       end
 
-      it "renders a pending template" do
-        expect(response).to render_template(:pending)
+      it "returns with 200 status code" do
         expect(response).to have_http_status(:ok)
       end
     end
 
     # * ALL
-    context "GET all" do
-      before do
-        get thaali_takhmeens_all_path(PREV_YR)
+    describe "GET /all" do
+      before { get thaali_takhmeens_all_path(PREV_YR) }
+
+      it "renders all template" do
+        expect(response).to render_template(:all)
       end
 
-      it "renders a all template" do
-        expect(response).to render_template(:all)
+      it "returns with 200 status code" do
         expect(response).to have_http_status(:ok)
       end
     end
   end
 
   # * Accessible by Admins & Members
-  context "'admin' & 'member' can access 👉" do
+  describe "'Admin' & 'Member' can access 👉" do
     before do
-      @user = create(:user_other_than_viewer, password: @password)
-      post signup_path, params: {sessions: @user.attributes.merge({password: @password})}
+      user = create(:user_other_than_viewer)
+      post signup_path, params: {sessions: user.attributes.merge({password: user.password})}
     end
 
     # * NEW
-    context "GET new" do
-      before do
-        @sabeel = create(:sabeel)
-      end
+    describe "GET /new" do
+      context "when sabeel is NOT currently taking thaali" do
+        let(:thaali) { create(:previous_takhmeen) }
 
-      context "if sabeel HAS NOT registered for currrent-year thaali" do
-        it "RENDERS a new template with 200 status code" do
-          get new_sabeel_takhmeen_path(@sabeel)
-          expect(response).to have_http_status(:ok)
+        before { get new_sabeel_takhmeen_path(thaali.sabeel) }
+
+        it "renders new template" do
           expect(response).to render_template(:new)
         end
 
-        context "and if sabeel has taken previous year thaali" do
-          before do
-            @prev_thaali = create(:previous_takhmeen, sabeel_id: @sabeel.id)
-            get new_sabeel_takhmeen_path(@sabeel)
-          end
+        it "returns with 200 status code" do
+          expect(response).to have_http_status(:ok)
+        end
 
-          it "shows 'number' & 'size' attribute values in the form" do
-            expect(response.body).to include(@prev_thaali.number.to_s)
-            expect(response.body).to include(@prev_thaali.size)
+        describe "but if it was previously registered" do
+          it "load the form with previous values" do
+            expect(response.body).to include(thaali.number.to_s)
           end
         end
       end
 
-      context "if sabeel HAS registered for currrent-year thaali" do
-        before do
-          @cur_thaali = create(:active_takhmeen, sabeel_id: @sabeel.id)
-        end
+      context "when sabeel has registered for currrent-year thaali" do
+        let(:sabeel) { create(:active_sabeel) }
 
         it "DOES NOT render new tempelate" do
-          get new_sabeel_takhmeen_path(@sabeel)
-          expect(response).to redirect_to sabeel_path(@sabeel)
+          get new_sabeel_takhmeen_path(sabeel)
+          expect(response).to redirect_to sabeel_path(sabeel)
         end
       end
     end
 
     # * CREATE
-    context "POST create" do
-      before do
-        @sabeel = create(:sabeel)
-      end
+    describe "POST /create" do
+      let(:sabeel) { create(:sabeel) }
 
       context "with valid attributes" do
         before do
-          @valid_attributes = attributes_for(:thaali_takhmeen)
-          post sabeel_takhmeens_path(sabeel_id: @sabeel.id), params: {thaali_takhmeen: @valid_attributes}
-          @thaali = @sabeel.thaali_takhmeens.first
-        end
-
-        it "creates a new Thaali" do
-          expect(@thaali).to be_truthy
+          valid_attributes = attributes_for(:thaali_takhmeen)
+          post sabeel_takhmeens_path(sabeel_id: sabeel.id), params: {thaali_takhmeen: valid_attributes}
         end
 
         it "redirects to created thaali" do
+          expect(response).to redirect_to takhmeen_path(sabeel.thaali_takhmeens.first)
+        end
+
+        it "returns with 302 redirect status code" do
           expect(response).to have_http_status(:found)
-          expect(response).to redirect_to takhmeen_path(@thaali)
         end
       end
 
       context "with invalid attributes" do
         before do
-          @invalid_attributes = attributes_for(:thaali_takhmeen, size: nil)
-          post sabeel_takhmeens_path(sabeel_id: @sabeel.id), params: {thaali_takhmeen: @invalid_attributes}
-          @thaali = @sabeel.thaali_takhmeens.first
-        end
-
-        it "does not create a new Thaali" do
-          expect(@thaali).to be_nil
+          invalid_attributes = attributes_for(:thaali_takhmeen, size: nil)
+          post sabeel_takhmeens_path(sabeel_id: sabeel.id), params: {thaali_takhmeen: invalid_attributes}
         end
 
         it "renders a new template" do
           expect(response).to render_template(:new)
+        end
+
+        it "returns with 422 status code" do
           expect(response).to have_http_status(:unprocessable_entity)
         end
       end
     end
 
     # * EDIT
-    context "GET edit" do
-      before do
-        @thaali = create(:thaali_takhmeen)
-        get edit_takhmeen_path(@thaali.id)
-      end
+    describe "GET /edit" do
+      let(:thaali) { create(:thaali_takhmeen) }
+
+      before { get edit_takhmeen_path(thaali.id) }
 
       it "renders render an edit template" do
         expect(response).to render_template(:edit)
+      end
+
+      it "returns with 200 status code" do
         expect(response).to have_http_status(:ok)
       end
 
       it "renders the instance that was passed in the params" do
         # it could be any attribute, not only size
-        expect(response.body).to include(@thaali.size)
+        expect(response.body).to include(thaali.size)
       end
     end
 
     # * UPDATE
-    context "PATCH update" do
-      before do
-        @thaali = create(:thaali_takhmeen)
-      end
+    describe "PATCH /update" do
+      let(:thaali) { create(:thaali_takhmeen) }
 
       context "with valid attributes" do
         before do
-          @thaali.number = Random.rand(1..100)
-          patch takhmeen_path(@thaali), params: {thaali_takhmeen: @thaali.attributes}
+          thaali.number = Random.rand(1..100)
+          patch takhmeen_path(thaali), params: {thaali_takhmeen: thaali.attributes}
         end
 
         it "redirects to updated thaali page" do
-          expect(response).to redirect_to takhmeen_path("#{@thaali.year}-#{@thaali.number}")
+          expect(response).to redirect_to takhmeen_path("#{thaali.year}-#{thaali.number}")
         end
       end
 
       context "with invalid attributes" do
         before do
-          @thaali.total = 0
-          patch takhmeen_path(@thaali), params: {thaali_takhmeen: @thaali.attributes}
+          thaali.total = 0
+          patch takhmeen_path(thaali), params: {thaali_takhmeen: thaali.attributes}
         end
 
         it "renders an edit template" do
@@ -227,38 +220,35 @@ RSpec.describe "ThaaliTakhmeen request - user type 👉" do
     end
 
     # * DESTROY
-    context "DELETE destroy" do
-      before do
-        @sabeel = create(:sabeel)
-        thaali = create(:thaali_takhmeen, sabeel_id: @sabeel.id)
-        delete takhmeen_path(thaali)
-        # find method will raise an error
-        @thaali = ThaaliTakhmeen.find_by(id: thaali.id)
-      end
+    describe "DELETE /destroy" do
+      let(:thaali) { create(:thaali_takhmeen) }
+
+      before { delete takhmeen_path(thaali) }
 
       it "destroys the thaali" do
-        expect(@thaali).to be_nil
+        expect(ThaaliTakhmeen.find_by(id: thaali.id)).to be_nil
       end
 
       it "redirects to its sabeel page" do
-        expect(response).to redirect_to sabeel_path(@sabeel)
+        expect(response).to redirect_to sabeel_path(thaali.sabeel)
       end
     end
   end
 
   # * NOT Accessible by Viewers
-  context "'viewer' CANNOT access 👉" do
-    before do
-      @viewer = create(:viewer_user, password: @password)
-      post signup_path, params: {sessions: @viewer.attributes.merge({password: @password})}
-      @sabeel = create(:sabeel)
+  describe "'Viewer' CANNOT access 👉" do
+    let(:sabeel) { create(:sabeel) }
+
+    # rubocop:disable RSpec/BeforeAfterAll
+    before(:all) do
+      viewer = create(:viewer_user)
+      post signup_path, params: {sessions: viewer.attributes.merge({password: viewer.password})}
     end
+    # rubocop:enable RSpec/BeforeAfterAll
 
     # * NEW
-    context "GET new" do
-      before do
-        get new_sabeel_takhmeen_path(@sabeel)
-      end
+    describe "GET /new" do
+      before { get new_sabeel_takhmeen_path(sabeel) }
 
       it "does not render a new template" do
         expect(response).not_to render_template(:new)
@@ -274,10 +264,10 @@ RSpec.describe "ThaaliTakhmeen request - user type 👉" do
     end
 
     # * EDIT
-    context "GET edit" do
+    describe "GET /edit" do
       before do
-        @thaali = create(:thaali_takhmeen)
-        get edit_takhmeen_path(@thaali)
+        thaali = create(:thaali_takhmeen)
+        get edit_takhmeen_path(thaali)
       end
 
       it "does not render a edit template" do
@@ -294,16 +284,13 @@ RSpec.describe "ThaaliTakhmeen request - user type 👉" do
     end
 
     # * DETROY
-    context "DELETE destroy" do
-      before do
-        thaali = create(:thaali_takhmeen, sabeel_id: @sabeel.id)
-        delete takhmeen_path(thaali)
-        # find method will raise an error
-        @thaali = ThaaliTakhmeen.find_by(id: thaali.id)
-      end
+    describe "DELETE /destroy" do
+      let(:thaali) { create(:active_takhmeen) }
+
+      before { delete takhmeen_path(thaali) }
 
       it "does not destroy the thaali" do
-        expect(@thaali).not_to be_nil
+        expect(ThaaliTakhmeen.find_by(id: thaali.id)).to be_persisted
       end
 
       it "redirects to the root path" do
