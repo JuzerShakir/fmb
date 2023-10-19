cd115d207e8a7130276591429f4be6eb8677a6c1
Juzer Shakir <juzershakir.webdev@gmail.com>
2023-10-19 23:54:25 +0530

    fix all rubocop errors & refactoring in spec/features/navbar_spec.rb

--- spec/features/navbar_spec.rb
+++ spec/features/navbar_spec.rb
@@ -3,229 +3,142 @@
 require "rails_helper"
 
 RSpec.describe "Navbar" do
-  context "all users should be shown" do
-    before do
-      @user = create(:user)
-      page.set_rack_session(user_id: @user.id)
-      visit root_path
-    end
+  # rubocop:disable RSpec/BeforeAfterAll
+  before do
+    page.set_rack_session(user_id: user.id)
+    visit root_path
+  end
+  # rubocop:enable RSpec/BeforeAfterAll
 
-    it "a title" do
-      within(".navbar-brand") do
-        expect(page).to have_content("FMB")
-      end
-    end
+  describe "'Anyone' who have logged in displays" do
+    let(:user) { create(:user) }
 
     # * Statistics
-    context "a dropdown for Statistics" do
-      it "with 'Sabeels' link" do
-        within("#statistics") do
-          expect(page).to have_content("Sabeels")
-          click_on "Sabeels"
-          expect(current_path).to eql stats_sabeels_path
-        end
+    describe "Statistics dropdown menu" do
+      it "has link of sabeels stats" do
+        within("#statistics") { expect(page).to have_link("Sabeels", href: stats_sabeels_path) }
       end
 
-      it "with 'Thaali Takhmeens' link" do
-        within("#statistics") do
-          expect(page).to have_content("Thaali Takhmeens")
-          click_on "Thaali Takhmeens"
-          expect(current_path).to eql takhmeens_stats_path
-        end
+      it "has link of thaali takhmeen stats" do
+        within("#statistics") { expect(page).to have_link("Thaali Takhmeen", href: takhmeens_stats_path) }
       end
     end
 
     # * Resources
-    context "a dropdown for Resources" do
-      it "with 'Sabeels' link" do
-        within("#resources") do
-          expect(page).to have_content("Sabeels")
-          click_on "Sabeels"
-          expect(current_path).to eql sabeels_path
-        end
+    describe "Resources dropdown menu" do
+      it "has link of sabeels index action" do
+        within("#resources") { expect(page).to have_link("Sabeels", href: sabeels_path) }
       end
 
-      it "with 'Thaali Takhmeens' link" do
-        within("#resources") do
-          expect(page).to have_content("Thaali Takhmeens")
-          click_on "Thaali Takhmeens"
-          expect(current_path).to eql root_path
-        end
+      it "has link of thaali takhmeen index action" do
+        within("#resources") { expect(page).to have_link("Thaali Takhmeens", href: root_path) }
       end
 
-      it "with 'Transactions' link" do
-        within("#resources") do
-          expect(page).to have_content("Transactions")
-          click_on "Transactions"
-          expect(current_path).to eql transactions_all_path
-        end
+      it "has link of transactions index action" do
+        within("#resources") { expect(page).to have_link("Transactions", href: transactions_all_path) }
       end
     end
   end
 
-  # * IS Admin
-  context "if user is an Admin" do
-    before do
-      @admin = create(:admin_user)
-      page.set_rack_session(user_id: @admin.id)
-      visit root_path
-    end
+  # * Admin
+  context "when admin" do
+    describe "has logged in, it displays" do
+      let(:user) { create(:admin_user) }
 
-    it "show 'Admin' dropdown" do
-      within(".navbar-nav") do
-        expect(page).to have_css("#admin", text: "Admin")
+      it "'Admin' dropdown" do
+        within(".navbar-nav") { expect(page).to have_css("#admin", text: "Admin") }
+      end
+
+      # * New Sabeel
+      it "'Create Sabeel' link" do
+        within(".navbar-nav") { expect(page).to have_link("Create Sabeel", href: new_sabeel_path) }
+      end
+
+      # * My Profile
+      it "'My Profile' link" do
+        within("#admin") { expect(page).to have_link("My Profile", href: user_path(user)) }
+      end
+
+      # * New User
+      it "'New User' link" do
+        within("#admin") { expect(page).to have_link("New User", href: new_user_path) }
+      end
+
+      # * Home
+      it "'Home' link" do
+        within("#admin") { expect(page).to have_link("Home", href: users_path) }
+      end
+
+      # * Log Out
+      it "'Log out' link" do
+        within("#admin") { expect(page).to have_link("Log out", href: destroy_path) }
       end
     end
 
-    # * New Sabeel
-    it "show 'Create Sabeel' link" do
-      within(".navbar-nav") do
-        expect(page).to have_content("Create Sabeel")
-        click_on "Create Sabeel"
-        expect(current_path).to eql new_sabeel_path
-      end
-    end
+    # * NOT Admin
+    describe "has NOT logged in, do NOT display" do
+      let(:user) { create(:user_other_than_admin) }
 
-    # * My Profile
-    it "show 'My Profile' link" do
-      within("#admin") do
-        expect(page).to have_content("My Profile")
-        click_on "My Profile"
-        expect(current_path).to eql user_path(@admin)
-      end
-    end
-
-    # * New User
-    it "show 'New User' link" do
-      within("#admin") do
-        expect(page).to have_content("New User")
-        click_on "New User"
-        expect(current_path).to eql new_user_path
-      end
-    end
-
-    # * Home
-    it "show 'Home' link" do
-      within("#admin") do
-        expect(page).to have_content("Home")
-        click_on "Home"
-        expect(current_path).to eql users_path
-      end
-    end
-
-    # * Log Out
-    it "show 'Log out' link" do
-      within("#admin") do
-        expect(page).to have_content("Log out")
-        click_on "Log out"
-        expect(current_path).to eql login_path
-      end
-    end
-  end
-
-  # * NOT Admin
-  context "if user is NOT an Admin, do NOT show" do
-    before do
-      @user = create(:user_other_than_admin)
-      page.set_rack_session(user_id: @user.id)
-      visit root_path
-    end
-
-    # * New Sabeel
-    it "'Create Sabeel' link" do
-      within(".navbar-nav") do
-        expect(page).not_to have_content("Create Sabeel")
-      end
-    end
-
-    # * Home
-    it "'Home' link" do
-      if @user.member?
-        within("#member") do
-          expect(page).not_to have_content("Home")
-        end
-      else
-        within("#viewer") do
-          expect(page).not_to have_content("Home")
-        end
-      end
-    end
-
-    # * New User
-    it "'New User' link" do
-      if @user.member?
-        within("#member") do
-          expect(page).not_to have_content("New User")
-        end
-      else
-        within("#viewer") do
-          expect(page).not_to have_content("New User")
+      # * New Sabeel
+      it "'Create Sabeel' link" do
+        within(".navbar-nav") do
+          expect(page).not_to have_content("Create Sabeel")
         end
       end
     end
   end
 
   # * Member
-  context "if user is a Member" do
-    before do
-      @member = create(:member_user)
-      page.set_rack_session(user_id: @member.id)
-      visit root_path
-    end
+  context "when Member, it displays" do
+    let(:user) { create(:member_user) }
 
-    it "show 'Member' dropdown" do
-      within(".navbar-nav") do
-        expect(page).to have_css("#member", text: "Member")
-      end
+    it "'Member' dropdown" do
+      within(".navbar-nav") { expect(page).to have_css("#member", text: "Member") }
     end
 
     # * My Profile
-    it "show 'My Profile' link" do
-      within("#member") do
-        expect(page).to have_content("My Profile")
-        click_on "My Profile"
-        expect(current_path).to eql user_path(@member)
-      end
+    it "'My Profile' link" do
+      within("#member") { expect(page).to have_link("My Profile", href: user_path(user)) }
+    end
+
+    it "NO 'New User' link" do
+      within("#member") { expect(page).not_to have_content("New User") }
+    end
+
+    it "NO 'Home' link" do
+      within("#member") { expect(page).not_to have_content("Home") }
     end
 
     # * Log Out
-    it "show 'Log out' link" do
-      within("#member") do
-        expect(page).to have_content("Log out")
-        click_on "Log out"
-        expect(current_path).to eql login_path
-      end
+    it "'Log out' link" do
+      within("#member") { expect(page).to have_link("Log out", href: destroy_path) }
     end
   end
 
   # * Viewer
-  context "if user is a Viewer" do
-    before do
-      @viewer = create(:viewer_user)
-      page.set_rack_session(user_id: @viewer.id)
-      visit root_path
-    end
+  context "when Viewer, it displays" do
+    let(:user) { create(:viewer_user) }
 
     it "have 'viewer' id" do
-      within(".navbar-nav") do
-        expect(page).to have_css("#viewer")
-      end
+      within(".navbar-nav") { expect(page).to have_css("#viewer") }
     end
 
     # * My Profile
-    it "do not show 'My Profile' link" do
-      within("#viewer") do
-        expect(page).not_to have_content("My Profile")
-      end
+    it "NO 'My Profile' link" do
+      within("#viewer") { expect(page).not_to have_content("My Profile") }
+    end
+
+    it "NO 'Home' link" do
+      within("#viewer") { expect(page).not_to have_content("Home") }
+    end
+
+    it "NO 'New User' link" do
+      within("#viewer") { expect(page).not_to have_content("New User") }
     end
 
     # * Log Out
-    it "show 'Log out' link" do
-      within("#viewer") do
-        expect(page).to have_content("Log out")
-        click_on "Log out"
-        expect(current_path).to eql login_path
-      end
+    it "'Log out' link" do
+      within("#viewer") { expect(page).to have_link("Log out", href: destroy_path) }
     end
   end
 end
