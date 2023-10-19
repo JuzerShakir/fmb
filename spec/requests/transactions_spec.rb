cd83356fe9ba913d4e73065beb808c66104d944d
Juzer Shakir <juzershakir.webdev@gmail.com>
2023-10-19 18:10:24 +0530

    fix all rubocop errors in spec/requests/transaction

--- spec/requests/transactions_spec.rb
+++ spec/requests/transactions_spec.rb
@@ -2,209 +2,203 @@
 
 require "rails_helper"
 
-RSpec.describe "Transaction request - user type 👉" do
-  before do
-    @password = Faker::Internet.password(min_length: 6, max_length: 72)
-  end
-
+RSpec.describe "Transaction request" do
   # * Accessible by all
-  context "any user can access 👉" do
-    before do
-      @user = create(:user, password: @password)
-      post signup_path, params: {sessions: @user.attributes.merge({password: @password})}
+  describe "'Anyone' who have logged in can access 👉" do
+    # rubocop:disable RSpec/BeforeAfterAll
+    before(:all) do
+      user = create(:user)
+      post signup_path, params: {sessions: user.attributes.merge({password: user.password})}
     end
+    # rubocop:enable RSpec/BeforeAfterAll
 
     # * ALL
-    context "GET ALL" do
-      before do
-        get transactions_all_path
+    describe "GET /all" do
+      before { get transactions_all_path }
+
+      it "renders all template" do
+        expect(response).to render_template(:all)
       end
 
-      it "renders a 'all' template" do
-        expect(response).to render_template(:all)
+      it "returns with 200 status code" do
         expect(response).to have_http_status(:ok)
       end
     end
 
     # * SHOW
-    context "GET show" do
-      before do
-        @transaction = create(:transaction)
-        get transaction_path(@transaction)
-      end
+    describe "GET /show" do
+      let(:transaction) { create(:transaction) }
+
+      before { get transaction_path(transaction) }
 
       it "renders a show template" do
         expect(response).to render_template(:show)
+      end
+
+      it "returns with 200 status code" do
         expect(response).to have_http_status(:ok)
       end
 
       it "renders the instance that was passed in the params" do
         # it could be any attribute, not only amount
-        amount = number_with_delimiter(@transaction.amount)
+        amount = number_with_delimiter(transaction.amount)
         expect(response.body).to include(amount)
       end
     end
   end
 
   # * Accessible by Admins & Members
-  context "'admin' & 'member' can access 👉" do
-    before do
-      @user = create(:user_other_than_viewer, password: @password)
-      post signup_path, params: {sessions: @user.attributes.merge({password: @password})}
+  describe "'Admin' & 'Member' can access 👉" do
+    # rubocop:disable RSpec/BeforeAfterAll
+    before(:all) do
+      user = create(:user_other_than_viewer)
+      post signup_path, params: {sessions: user.attributes.merge({password: user.password})}
     end
+    # rubocop:enable RSpec/BeforeAfterAll
 
     # * NEW
-    context "GET new" do
-      before do
-        @sabeel = create(:sabeel)
-      end
+    describe "GET /new" do
+      context "when thaali_takhmeen IS COMPLETED" do
+        let(:thaali) { create(:completed_takhmeens) }
 
-      context "if thaali_takhmeen IS COMPLETED" do
-        before do
-          @thaali = create(:completed_takhmeens, sabeel_id: @sabeel.id)
-          get new_takhmeen_transaction_path(@thaali)
-        end
+        before { get new_takhmeen_transaction_path(thaali) }
 
         it "DOES NOT render new tempelate" do
-          expect(response).to redirect_to takhmeen_path(@thaali)
+          expect(response).to redirect_to takhmeen_path(thaali)
         end
       end
 
-      context "if thaali_takhmeen IS NOT COMPLETED" do
-        before do
-          @thaali = create(:thaali_takhmeen, sabeel_id: @sabeel.id)
-          get new_takhmeen_transaction_path(@thaali)
+      context "when thaali_takhmeen IS NOT COMPLETED" do
+        let(:thaali) { create(:thaali_takhmeen) }
+
+        before { get new_takhmeen_transaction_path(thaali) }
+
+        it "returns new template" do
+          expect(response).to render_template(:new)
         end
 
-        it "returns a 200 (OK) status code" do
+        it "returns with 200 status code" do
           expect(response).to have_http_status(:ok)
-          expect(response).to render_template(:new)
         end
       end
     end
 
     # * CREATE
-    context "POST create" do
-      before do
-        @thaali = create(:thaali_takhmeen)
-      end
+    describe "POST /create" do
+      let(:thaali) { create(:thaali_takhmeen) }
 
-      context "with valid attributes" do
+      context "with valid values" do
         before do
-          @valid_attributes = attributes_for(:transaction)
-          post takhmeen_transactions_path(@thaali), params: {transaction: @valid_attributes}
-          @transaction = Transaction.find_by(recipe_no: @valid_attributes[:recipe_no])
+          valid_attributes = attributes_for(:transaction)
+          post takhmeen_transactions_path(thaali), params: {transaction: valid_attributes}
         end
 
-        it "creates a new Transaction" do
-          expect(@transaction).to be_truthy
+        it "returns with 302 redirect status code" do
+          expect(response).to have_http_status(:found)
         end
 
         it "redirects to created Transaction" do
-          expect(response).to have_http_status(:found)
-          expect(response).to redirect_to @transaction
+          expect(response).to redirect_to transaction_path(thaali.transactions.first)
         end
       end
 
-      context "with invalid attributes" do
+      context "with invalid values" do
         before do
-          @invalid_attributes = attributes_for(:transaction, amount: nil)
-          post takhmeen_transactions_path(@thaali), params: {transaction: @invalid_attributes}
-          @transaction = Transaction.find_by(recipe_no: @invalid_attributes[:recipe_no])
-        end
-
-        it "does not create a new Transaction" do
-          expect(@transaction).to be_nil
+          invalid_attributes = attributes_for(:transaction, amount: nil)
+          post takhmeen_transactions_path(thaali), params: {transaction: invalid_attributes}
         end
 
         it "renders a new template" do
           expect(response).to render_template(:new)
+        end
+
+        it "returns with 422 status code" do
           expect(response).to have_http_status(:unprocessable_entity)
         end
       end
     end
 
     # * EDIT
-    context "GET edit" do
-      before do
-        @transaction = create(:transaction)
-        get edit_transaction_path(@transaction)
+    describe "GET /edit" do
+      let(:transaction) { create(:transaction) }
+
+      before { get edit_transaction_path(transaction) }
+
+      it "renders an edit template" do
+        expect(response).to render_template(:edit)
       end
 
-      it "renders render an edit template" do
-        expect(response).to render_template(:edit)
+      it "returns with 200 status code" do
         expect(response).to have_http_status(:ok)
       end
 
       it "renders the instance that was passed in the params" do
         # it could be any attribute
-        expect(response.body).to include(@transaction.recipe_no.to_s)
+        expect(response.body).to include(transaction.recipe_no.to_s)
       end
     end
 
     # * UPDATE
-    context "PATCH update" do
-      before do
-        @transaction = create(:transaction)
-      end
+    describe "PATCH /update" do
+      let(:transaction) { create(:transaction) }
 
       context "with valid attributes" do
         before do
-          @transaction.amount = Random.rand(1..100)
-          patch transaction_path(@transaction), params: {transaction: @transaction.attributes}
+          transaction.amount = Random.rand(1..100)
+          patch transaction_path(transaction), params: {transaction: transaction.attributes}
         end
 
         it "redirects to updated Transaction page" do
-          expect(response).to redirect_to @transaction
+          expect(response).to redirect_to transaction
         end
       end
 
       context "with invalid attributes" do
         before do
-          @transaction.recipe_no = -123
-          patch transaction_path(@transaction), params: {transaction: @transaction.attributes}
+          transaction.recipe_no = -123
+          patch transaction_path(transaction), params: {transaction: transaction.attributes}
         end
 
-        it "renders an edit template" do
+        it "renders edit template" do
           expect(response).to render_template(:edit)
+        end
+
+        it "returns with 422 status code" do
           expect(response).to have_http_status(:unprocessable_entity)
         end
       end
     end
 
     # * DESTROY
-    context "DELETE destroy" do
-      before do
-        @thaali = create(:thaali_takhmeen)
-        transaction = create(:transaction, thaali_takhmeen_id: @thaali.id)
-        delete transaction_path(transaction)
-        # find method will raise an error
-        @transaction = Transaction.find_by(id: transaction.id)
-      end
+    describe "DELETE /destroy" do
+      let(:transaction) { create(:transaction) }
+
+      before { delete transaction_path(transaction) }
 
       it "destroys the Transaction" do
-        expect(@transaction).to be_nil
+        expect(Transaction.find_by(id: transaction.id)).to be_nil
       end
 
       it "redirects to its parent Thaali Takhmeen page" do
-        expect(response).to redirect_to takhmeen_path(@thaali)
+        expect(response).to redirect_to takhmeen_path(transaction.thaali_takhmeen)
       end
     end
   end
 
   # * NOT Accessible by Viewers
-  context "'viewer' CANNOT access 👉" do
-    before do
-      @viewer = create(:viewer_user, password: @password)
-      post signup_path, params: {sessions: @viewer.attributes.merge({password: @password})}
+  describe "'Viewer' CANNOT access 👉" do
+    # rubocop:disable RSpec/BeforeAfterAll
+    before(:all) do
+      viewer = create(:viewer_user)
+      post signup_path, params: {sessions: viewer.attributes.merge({password: viewer.password})}
     end
+    # rubocop:enable RSpec/BeforeAfterAll
 
     # * NEW
-    context "GET new" do
-      before do
-        thaali = create(:completed_takhmeens)
-        get new_takhmeen_transaction_path(thaali)
-      end
+    describe "GET /new" do
+      let(:thaali) { create(:completed_takhmeens) }
+
+      before { get new_takhmeen_transaction_path(thaali) }
 
       it "does not render a new template" do
         expect(response).not_to render_template(:new)
@@ -220,11 +214,10 @@ RSpec.describe "Transaction request - user type 👉" do
     end
 
     # * EDIT
-    context "GET edit" do
-      before do
-        transaction = create(:transaction)
-        get edit_transaction_path(transaction)
-      end
+    describe "GET /edit" do
+      let(:transaction) { create(:transaction) }
+
+      before { get edit_transaction_path(transaction) }
 
       it "does not render a edit template" do
         expect(response).not_to render_template(:edit)
@@ -240,17 +233,13 @@ RSpec.describe "Transaction request - user type 👉" do
     end
 
     # * DETROY
-    context "DELETE destroy" do
-      before do
-        thaali = create(:thaali_takhmeen)
-        transaction = create(:transaction, thaali_takhmeen_id: thaali.id)
-        delete transaction_path(transaction)
-        # find method will raise an error
-        @transaction = Transaction.find_by(id: transaction.id)
-      end
+    describe "DELETE /destroy" do
+      let(:transaction) { create(:transaction) }
+
+      before { delete transaction_path(transaction) }
 
       it "does not destroy the thaali" do
-        expect(@transaction).not_to be_nil
+        expect(Transaction.find_by(id: transaction.id)).to be_persisted
       end
 
       it "redirects to the root path" do
