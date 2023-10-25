30e717a960e43b5db0126291977df762f088a354
Juzer Shakir <juzershakir.webdev@gmail.com>
2023-10-25 09:47:35 +0530

    shift logic session request test to new path

---
+++ spec/requests/sessions/new_spec.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe "Session New request" do
+  let(:user) { create(:user) }
+
+  # * ACCESSIBLE
+  context "when made by logged out user" do
+    before { get login_path }
+
+    it { expect(response).to render_template(:new) }
+    it { expect(response).to have_http_status(:ok) }
+  end
+
+  # * NOT ACCESSIBLE
+  context "when made by logged in users" do
+    before do
+      post signup_path, params: {sessions: user.attributes.merge({password: user.password})}
+      get login_path
+    end
+
+    it { expect(response).to redirect_to root_path }
+    it { expect(response).to have_http_status(:found) }
+  end
+end
