94cab6cbf8a1818fb704e3103d76da1587affd39
Juzer Shakir <juzershakir.webdev@gmail.com>
2023-10-25 12:51:43 +0530

    divide sabeel request tests with respect to their actions

---
+++ spec/requests/sabeels/inactive_spec.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe "Thaali Inactive request" do
+  let(:apartment) { Sabeel.apartments.keys.sample }
+
+  # * NOT ACCESSIBLE
+  context "when made by logged out user" do
+    before { get sabeels_inactive_path(apartment) }
+
+    it { expect(response).to have_http_status(:found) }
+    it { expect(response).to redirect_to login_path }
+  end
+
+  # * ACCESSIBLE
+  context "when made by any logged in user" do
+    let(:user) { create(:user) }
+
+    before do
+      post signup_path, params: {sessions: user.attributes.merge({password: user.password})}
+      get sabeels_inactive_path(apartment)
+    end
+
+    it { expect(response).to render_template(:inactive) }
+    it { expect(response).to have_http_status(:ok) }
+  end
+end
