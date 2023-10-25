578301c5a3f39b0732ac3eab6a1243d76c2dc5df
Juzer Shakir <juzershakir.webdev@gmail.com>
2023-10-25 12:16:58 +0530

    divide thaali request tests with respect to their actions

---
+++ spec/requests/thaali_takhmeens/edit_spec.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe "Thaali Edit request" do
+  let(:thaali) { create(:thaali_takhmeen) }
+
+  # * NOT ACCESSIBLE
+  context "when made by logged out user" do
+    before { get edit_takhmeen_path(thaali) }
+
+    it { expect(response).to have_http_status(:found) }
+    it { expect(response).to redirect_to login_path }
+  end
+
+  context "when made by -" do
+    before do
+      post signup_path, params: {sessions: user.attributes.merge({password: user.password})}
+      get edit_takhmeen_path(thaali)
+    end
+
+    # * NOT ACCESSIBLE
+    describe "Viewer" do
+      let(:user) { create(:viewer_user) }
+
+      it { expect(response).to have_http_status(:found) }
+      it { expect(response).to redirect_to root_path }
+    end
+
+    # * ACCESSIBLE
+    describe "Admin or Member" do
+      let(:user) { create(:user_other_than_viewer) }
+
+      it { expect(response).to render_template(:edit) }
+      it { expect(response).to have_http_status(:ok) }
+    end
+  end
+end
---
+++ spec/requests/thaali_takhmeens/pending_spec.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe "Thaali Pending request" do
+  # * NOT ACCESSIBLE
+  context "when made by logged out user" do
+    before { get thaali_takhmeens_pending_path(PREV_YR) }
+
+    it { expect(response).to have_http_status(:found) }
+    it { expect(response).to redirect_to login_path }
+  end
+
+  # * ACCESSIBLE
+  context "when made by any logged in user" do
+    let(:user) { create(:user) }
+
+    before do
+      post signup_path, params: {sessions: user.attributes.merge({password: user.password})}
+      get thaali_takhmeens_pending_path(PREV_YR)
+    end
+
+    it { expect(response).to render_template(:pending) }
+    it { expect(response).to have_http_status(:ok) }
+  end
+end
