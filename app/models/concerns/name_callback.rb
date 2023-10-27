9d81b59476cc1c3b3aea0aaca06722e140a7c7c2
Juzer Shakir <juzershakir.webdev@gmail.com>
2023-10-27 10:35:32 +0530

    make 'capitalize_name' private method

--- app/models/concerns/name_callback.rb
+++ app/models/concerns/name_callback.rb
@@ -5,6 +5,8 @@ module NameCallback
     before_save :capitalize_name, if: :will_save_change_to_name?
   end
 
+  private
+
   def capitalize_name
     self.name = name.split.map(&:capitalize).join(" ")
   end
