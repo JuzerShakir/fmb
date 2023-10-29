fd4bb7d72cead29fb5bc79790fd52d5172dba84e
Juzer Shakir <juzershakir.webdev@gmail.com>
2023-10-29 23:37:47 +0530

    remove redundant paid attribute

--- db/migrate/20221217053403_create_thaalis.rb
+++ db/migrate/20221217053403_create_thaalis.rb
@@ -4,7 +4,6 @@ class CreateThaalis < ActiveRecord::Migration[7.0]
       t.references :sabeel, null: false, foreign_key: true
       t.integer :year, null: false
       t.integer :total, null: false
-      t.integer :paid, null: false, default: 0
       t.integer :number, null: false
       t.integer :size, null: false
       t.string :slug, null: false, index: {unique: true}
