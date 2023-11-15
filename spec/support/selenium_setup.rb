ebb5afafd3c6f43bc6fe180d163f1fec7c3e7460
Juzer Shakir <juzershakir.webdev@gmail.com>
2023-11-15 20:11:47 +0530

    use chrome browser

--- spec/support/selenium_setup.rb
+++ spec/support/selenium_setup.rb
@@ -1,8 +1,9 @@
-Capybara.register_driver :headless_firefox do |app|
-  capabilities = Selenium::WebDriver::Remote::Capabilities.firefox(
-    "moz:firefoxOptions": {args: %w[-headless -disable-gpu]}
-  )
-  Capybara::Selenium::Driver.new(app, browser: :firefox, options: capabilities)
+Capybara.register_driver :headless_chrome do |app|
+  options = Selenium::WebDriver::Chrome::Options.new
+  options.args << "--headless"
+  options.args << "--disable-gpu"
+
+  Capybara::Selenium::Driver.new(app, browser: :chrome, options:)
 end
 
-Capybara.javascript_driver = :headless_firefox
+Capybara.javascript_driver = :headless_chrome
