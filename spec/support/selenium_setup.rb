4602f89a00d176a7a10ca97d1d21a88843ee78fa
Juzer Shakir <juzershakir.webdev@gmail.com>
2023-11-15 23:30:00 +0530

    update comment

--- spec/support/selenium_setup.rb
+++ spec/support/selenium_setup.rb
@@ -2,7 +2,7 @@ Capybara.register_driver :headless_chrome do |app|
   options = Selenium::WebDriver::Chrome::Options.new
   options.args << "--headless"
   options.args << "--disable-gpu"
-  # downloads pdf by default, so setting invalid directory will discard it
+  # PDF downloads by default; invalid directory blocks it
   options.add_preference("download.default_directory", "/path/to/invalid/directory")
 
   Capybara::Selenium::Driver.new(app, browser: :chrome, options:)
