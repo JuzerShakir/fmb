d340db0d438f407f4e1f1d59180e56bc79d83ac3
Juzer Shakir <juzershakir.webdev@gmail.com>
2023-11-20 12:09:32 +0530

    use horizontal forms by default

--- config/initializers/simple_form_bootstrap.rb
+++ config/initializers/simple_form_bootstrap.rb
@@ -404,7 +404,7 @@ SimpleForm.setup do |config|
   end
 
   # The default wrapper to be used by the FormBuilder.
-  config.default_wrapper = :vertical_form
+  config.default_wrapper = :horizontal_form
 
   # Custom wrappers for input types. This should be a hash containing an input
   # type as key and the wrapper that will be used for all inputs with specified type.
