d0b756718a340202697dd1ecfcba5e729c67f6ac
Juzer Shakir <juzershakir.webdev@gmail.com>
2023-11-23 00:10:45 +0530

    reorder alphabetically

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,30 @@
 module ApplicationHelper
   include Pagy::Frontend
 
+  def contact_email
+    options = set_url("mailto:juzershakir.webdev@gmail.com")
+
+    content_tag(:a, "", options) do
+      font_awesome_helper(" Email", "fa-regular fa-envelope")
+    end
+  end
+
+  def contact_telegram
+    options = set_url("https://t.me/juzershakir")
+
+    content_tag(:a, "", options) do
+      font_awesome_helper(" Telegram", "fa-brands fa-telegram")
+    end
+  end
+
+  def contact_whatsapp
+    options = set_url("https://wa.me/919819393148")
+
+    content_tag(:a, "", options) do
+      font_awesome_helper(" WhatsApp", "fa-brands fa-whatsapp")
+    end
+  end
+
   def render_flash_message(type, msg)
     logo = case type
     when "success" then "check"
@@ -16,30 +40,6 @@ module ApplicationHelper
   def rupees(num)
     content_tag :span do
       font_awesome_helper(number_with_delimiter(num), "fa-indian-rupee-sign fa-sm")
-    end
-  end
-
-  def contact_email
-    options = set_url("mailto:juzershakir.webdev@gmail.com")
-
-    content_tag(:a, "", options) do
-      font_awesome_helper(" Email", "fa-regular fa-envelope")
-    end
-  end
-
-  def contact_whatsapp
-    options = set_url("https://wa.me/919819393148")
-
-    content_tag(:a, "", options) do
-      font_awesome_helper(" WhatsApp", "fa-brands fa-whatsapp")
-    end
-  end
-
-  def contact_telegram
-    options = set_url("https://t.me/juzershakir")
-
-    content_tag(:a, "", options) do
-      font_awesome_helper(" Telegram", "fa-brands fa-telegram")
     end
   end
 
