0d896ce5a0531b5b7f9e140464f88f14ffaf0f18
Juzer Shakir <juzershakir.webdev@gmail.com>
2023-11-23 00:15:05 +0530

    use `set_url` method

--- app/helpers/sabeels_helper.rb
+++ app/helpers/sabeels_helper.rb
@@ -1,11 +1,6 @@
 module SabeelsHelper
   def pdf_button
-    options = {
-      href: sabeels_active_url(@apt, format: :pdf),
-      class: "default-btn btn-secondary",
-      target: :_blank,
-      rel: :noopener
-    }
+    options = set_url(sabeels_active_url(@apt, format: :pdf))
 
     content_tag(:a, "", options) do
       font_awesome_helper(" Generate PDF", "fa-file-pdf fa-lg")
