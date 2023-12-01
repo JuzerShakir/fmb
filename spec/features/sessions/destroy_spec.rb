faa498aeb1129929598c55d662eec035162c8177
Juzer Shakir <juzershakir.webdev@gmail.com>
2023-12-02 02:22:39 +0530

    show navbar to logged out users

--- spec/features/sessions/destroy_spec.rb
+++ spec/features/sessions/destroy_spec.rb
@@ -14,8 +14,4 @@ RSpec.describe "Sessions destroy" do
   it { expect(page).to have_current_path login_path }
 
   it { expect(page).to have_content("Logged out") }
-
-  it "does not show the navbar" do
-    expect(page).not_to have_css(".navbar")
-  end
 end
