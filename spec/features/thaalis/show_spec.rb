a37ad587fa488c934e2cd8d01d940c8c0f6c15ec
Juzer Shakir <juzershakir.webdev@gmail.com>
2023-12-09 16:19:32 +0530

    show 'Takhmeen Complete' message if dues cleared

--- spec/features/thaalis/show_spec.rb
+++ spec/features/thaalis/show_spec.rb
@@ -18,6 +18,29 @@ RSpec.describe "Thaali show template" do
       it { expect(page).to have_content(thaali.size.humanize) }
       it { expect(page).to have_content(number_with_delimiter(thaali.total)) }
       it { expect(page).to have_content(number_with_delimiter(thaali.balance)) }
+      it { expect(page).to have_content(number_with_delimiter(thaali.paid)) }
+
+      describe "if its dues are cleared" do
+        let(:thaali) { create(:taking_thaali_dues_cleared) }
+
+        it "balance amount is not shown" do
+          within("#payment-summary") do
+            expect(page).not_to have_content(number_with_delimiter(thaali.balance))
+          end
+        end
+
+        it "paid amount is not shown" do
+          within("#payment-summary") do
+            expect(page).not_to have_content(number_with_delimiter(thaali.paid))
+          end
+        end
+
+        it "a prompt is shown" do
+          within("#payment-summary") do
+            expect(page).to have_content("Takhmeen Complete")
+          end
+        end
+      end
     end
 
     describe "transaction details" do
