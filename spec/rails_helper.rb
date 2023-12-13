39d752bfe78650a6f216b670bac74045d470d3ca
Juzer Shakir <juzershakir.webdev@gmail.com>
2023-12-13 17:43:21 +0530

    require all libraries in `rails_helper` file

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -6,9 +6,11 @@ ENV["RAILS_ENV"] ||= "test"
 require_relative "../config/environment"
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
+# Add additional requires below this line. Rails is not loaded until this point!
 require "rspec/rails"
 require "capybara/rspec"
-# Add additional requires below this line. Rails is not loaded until this point!
+require "rack_session_access/capybara"
+require "action_view"
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
@@ -33,6 +35,8 @@ rescue ActiveRecord::PendingMigrationError => e
   abort e.to_s.strip
 end
 RSpec.configure do |config|
+  include ActionView::Helpers::NumberHelper
+  include ActionView::Helpers::DateHelper
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = Rails.root.join("spec/fixtures").to_s
 
