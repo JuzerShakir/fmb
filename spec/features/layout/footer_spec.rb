a97e4d2919cfadfeeeee40ed4e2220df66e4c2f0
Juzer Shakir <juzershakir.webdev@gmail.com>
2023-12-28 13:08:03 +0530

    add `Contact` & `About` links to footer

--- spec/features/layout/footer_spec.rb
+++ spec/features/layout/footer_spec.rb
@@ -5,6 +5,30 @@ require "rails_helper"
 RSpec.describe "Footer displays" do
   before { visit login_path }
 
+  it "contact" do
+    within("footer") { expect(page).to have_content("Contact") }
+  end
+
+  describe "contact of" do
+    before { click_link "Contact" }
+
+    it "email" do
+      within("footer") { expect(page).to have_link(nil, href: "mailto:juzershakir.webdev@gmail.com") }
+    end
+
+    it "whatsapp" do
+      within("footer") { expect(page).to have_link(nil, href: "https://wa.me/919819393148") }
+    end
+
+    it "telegram" do
+      within("footer") { expect(page).to have_link(nil, href: "https://t.me/juzershakir") }
+    end
+  end
+
+  it "about" do
+    within("footer") { expect(page).to have_link("About", href: "https://www.thedawoodibohras.com/about-the-bohras/faiz-al-mawaid-al-burhaniyah/") }
+  end
+
   it "dynamic year" do
     within("footer") { expect(page).to have_content(Date.current.year) }
   end
