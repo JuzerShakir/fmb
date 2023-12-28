4a698b4acbcc5456717340a6771756ebba08d3a6
Juzer Shakir <juzershakir.webdev@gmail.com>
2023-12-28 18:49:18 +0530

    remove tests testing duplicate logic

--- spec/features/sessions/new_spec.rb
+++ spec/features/sessions/new_spec.rb
@@ -5,54 +5,41 @@ require "rails_helper"
 RSpec.describe "Sessions New template" do
   let(:user) { create(:user) }
 
-  context "when user is not logged in" do
-    before { visit login_path }
+  before { visit login_path }
 
-    it { expect(page).to have_title "Login" }
+  it { expect(page).to have_title "Login" }
 
-    it "shows footer" do
-      expect(page).to have_css("#footer")
-    end
-
-    describe "logging in" do
-      before { fill_in "sessions_its", with: user.its }
-
-      context "with valid credentials" do
-        before do
-          fill_in "sessions_password", with: user.password
-          click_button "Login"
-        end
-
-        it "redirects to root path after login" do
-          expect(page).to have_current_path root_path, ignore_query: true
-        end
-
-        it "displays welcome message" do
-          expect(page).to have_content("Afzalus Salaam")
-        end
-      end
-
-      context "with invalid credentials" do
-        before do
-          fill_in "sessions_password", with: ""
-          click_button "Login"
-        end
-
-        it "displays validation error" do
-          expect(page).to have_content("Invalid credentials")
-        end
-      end
-    end
+  it "shows footer" do
+    expect(page).to have_css("#footer")
   end
 
-  context "when user is logged in" do
-    before do
-      page.set_rack_session(user_id: user.id)
-      visit login_path
+  describe "logging in" do
+    before { fill_in "sessions_its", with: user.its }
+
+    context "with valid credentials" do
+      before do
+        fill_in "sessions_password", with: user.password
+        click_button "Login"
+      end
+
+      it "redirects to root path after login" do
+        expect(page).to have_current_path root_path, ignore_query: true
+      end
+
+      it "displays welcome message" do
+        expect(page).to have_content("Afzalus Salaam")
+      end
     end
 
-    it "will redirect to root path" do
-      expect(page).to have_current_path root_path, ignore_query: true
+    context "with invalid credentials" do
+      before do
+        fill_in "sessions_password", with: ""
+        click_button "Login"
+      end
+
+      it "displays validation error" do
+        expect(page).to have_content("Invalid credentials")
+      end
     end
   end
 end
