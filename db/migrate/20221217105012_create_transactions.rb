c9493477cd281e0467cefd080d68d08514116ae4
Juzer Shakir <juzershakir.webdev@gmail.com>
2024-04-30 11:58:18 +0530

    BUG FIX: revert column name

--- db/migrate/20221217105012_create_transactions.rb
+++ db/migrate/20221217105012_create_transactions.rb
@@ -2,7 +2,7 @@ class CreateTransactions < ActiveRecord::Migration[7.0]
   def change
     create_table :transactions do |t|
       t.references :thaali, null: false, foreign_key: true
-      t.integer :receipt_number, null: false, index: {unique: true}
+      t.integer :recipe_no, null: false, index: {unique: true}
       t.integer :mode, null: false
       t.integer :amount, null: false
       t.date :date, null: false
