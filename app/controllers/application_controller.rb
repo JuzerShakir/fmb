18ead67610c8b48fa43d062974ff4acb8acb0e46
Juzer Shakir <juzershakir.webdev@gmail.com>
2024-12-11 16:40:40 +0530

    Allow only modern browser versions to navigate app

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,7 @@
 class ApplicationController < ActionController::Base
+  # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
+  allow_browser versions: :modern
+
   include Pagy::Backend
   add_flash_types :success, :notice
   helper_method :current_user
