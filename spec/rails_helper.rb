2fbb582c0972abaa91a0418261879122acd5b6e5
Juzer Shakir <juzershakir.webdev@gmail.com>
2024-12-21 23:48:26 +0530

    Add manual sign in setup for feature test

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -67,6 +67,9 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+  # Add custom sign in helper
+  config.include SignInHelper, type: :feature
 end
 
 Shoulda::Matchers.configure do |config|
