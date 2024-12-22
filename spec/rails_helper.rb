f03d4024434b54c42f30c236ce27015ad097f046
Juzer Shakir <juzershakir.webdev@gmail.com>
2024-12-22 05:49:45 +0530

    remove un-required modules

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -9,8 +9,6 @@ abort("The Rails environment is running in production mode!") if Rails.env.produ
 # Add additional requires below this line. Rails is not loaded until this point!
 require "rspec/rails"
 require "capybara/rspec"
-require "rack_session_access/capybara"
-require "action_view"
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
