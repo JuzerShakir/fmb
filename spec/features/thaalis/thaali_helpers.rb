85454f963545ece661e57668331123082e7cc95b
Juzer Shakir <juzershakir.webdev@gmail.com>
2024-12-22 12:04:58 +0530

    Remove dead code

--- spec/features/thaalis/thaali_helpers.rb
+++
@@ -1,9 +0,0 @@
-# shared_examples "view thaali records" do
-#   it "thaali number, balance and sabeel name", :js do
-#     thaalis.each do
-#       expect(page).to have_content(_1.number)
-#       expect(page).to have_content(_1.sabeel_name)
-#       expect(page).to have_content(number_to_human(_1.balance, precision: 1, round_mode: :down, significant: false, format: "%n%u", units: {thousand: "K", million: "M"}))
-#     end
-#   end
-# end
